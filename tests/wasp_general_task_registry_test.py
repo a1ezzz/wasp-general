e0c14f14fa39f06625405f2a9d9f7715e144f46b
Ildar Gafurov <dev@binblob.com>
2016-09-06 20:01:47 +0300

    wasp_general/task/registry.py: Task registry added (documented and tested)

--- tests/wasp_general_task_registry_test.py
+++ tests/wasp_general_task_registry_test.py
@@ -1,26 +1,149 @@
 # -*- coding: utf-8 -*-
 
-from wasp_general.task.sync import WSyncTask
-from wasp_general.task.registry import WTaskRegistry, WRegisteredTask, WTaskRegistryStorage
+import pytest
+
+from wasp_general.task.registry import WRegisteredTask, WTaskRegistryBase, WTaskRegistryStorage, WTaskRegistry
+from wasp_general.task.base import WTask
 
 
-class TestSnippet:
+def test_abstract_classes():
+	pytest.raises(TypeError, WTaskRegistryBase)
+	pytest.raises(NotImplementedError, WTaskRegistryBase.add, None, None)
+	pytest.raises(NotImplementedError, WTaskRegistryBase.remove, None, None)
+	pytest.raises(NotImplementedError, WTaskRegistryBase.clear, None)
+	pytest.raises(NotImplementedError, WTaskRegistryBase.tasks, None, None)
+	pytest.raises(NotImplementedError, WTaskRegistryBase.count, None)
 
-	def test_a(self):
 
-		assert(WTaskRegistry.__registry_storage__ is None)
+class TestWRegisteredTask:
 
-		class TempTaskRegistry(WTaskRegistry):
-			__registry_storage__ = WTaskRegistryStorage()
+	def test_task(self):
 
-		assert(TempTaskRegistry.__registry_storage__.count() == 0)
-
-		class TaskA(WSyncTask, metaclass=WRegisteredTask):
-			__registry__ = TempTaskRegistry
-
-			def _start(self):
+		with pytest.raises(ValueError):
+			class T1(metaclass=WRegisteredTask):
 				pass
 
-		assert(WTaskRegistry.__registry_storage__ is None)
-		assert(TempTaskRegistry.__registry_storage__.count() == 1)
-		assert(TempTaskRegistry.__registry_storage__.tasks(None)[0] == TaskA)
+		class FakeR:
+			pass
+
+		with pytest.raises(TypeError):
+			class T2(metaclass=WRegisteredTask):
+				__registry__ = FakeR
+
+		class R(WTaskRegistry):
+			__registry_storage__ = WTaskRegistryStorage()
+
+		assert(R.__registry_storage__.count() == 0)
+
+		with pytest.raises(TypeError):
+			class T3(metaclass=WRegisteredTask):
+				__registry__ = R
+
+		class T4(WTask, metaclass=WRegisteredTask):
+			__registry__ = R
+
+		assert(R.__registry_storage__.count() == 1)
+
+
+class TestWTaskRegistryStorage:
+
+	def test_add_remove_clear_count_tasks(self):
+
+		registry_storage = WTaskRegistryStorage()
+
+		class R(WTaskRegistry):
+			__registry_storage__ = WTaskRegistryStorage()
+
+		with pytest.raises(TypeError):
+			class FakeT1:
+				pass
+			registry_storage.add(FakeT1)
+
+		with pytest.raises(TypeError):
+			class FakeT2(WTask):
+				pass
+			registry_storage.add(FakeT2)
+
+		class T1(WTask, metaclass=WRegisteredTask):
+			__registry__ = R
+
+		class T2(WTask, metaclass=WRegisteredTask):
+			__registry__ = R
+
+		assert(registry_storage.count() == 0)
+		registry_storage.add(T1)
+		assert(registry_storage.count() == 1)
+		registry_storage.add(T1)
+		assert(registry_storage.count() == 2)
+		registry_storage.add(T2)
+		assert(registry_storage.count() == 3)
+
+		assert(len(list(filter(lambda x: x == T1, registry_storage.tasks(None)))) == 2)
+		assert(len(list(filter(lambda x: x == T2, registry_storage.tasks(None)))) == 1)
+
+		registry_storage.clear()
+		assert(registry_storage.count() == 0)
+
+		registry_storage.__multiple_tasks_per_tag__ = False
+		registry_storage.add(T1)
+		assert(registry_storage.count() == 1)
+
+		with pytest.raises(RuntimeError):
+			registry_storage.add(T1)
+
+		with pytest.raises(RuntimeError):
+			registry_storage.add(T2)
+
+		class T3(WTask, metaclass=WRegisteredTask):
+			__registry__ = R
+			__registry_tag__ = 1
+
+		registry_storage.add(T3)
+		assert(registry_storage.count() == 2)
+
+		assert(registry_storage.count() == 2)
+		assert(registry_storage.tasks(1) == T3)
+		assert(registry_storage.tasks(None) == T1)
+		registry_storage.remove(T3)
+		assert(registry_storage.count() == 1)
+		assert(registry_storage.tasks(None) == T1)
+		assert(registry_storage.tasks(1) is None)
+
+		assert(registry_storage.tasks(2) is None)
+
+
+class TestWTaskRegistry:
+	def test_registry(self):
+
+		class R1(WTaskRegistry):
+			pass
+
+		with pytest.raises(ValueError):
+			R1.registry_storage()
+
+		with pytest.raises(TypeError):
+			class FakeRS:
+				pass
+			R1.__registry_storage__ = FakeRS()
+			R1.registry_storage()
+
+		R1.__registry_storage__ = WTaskRegistryStorage()
+		assert(R1.registry_storage() == R1.__registry_storage__)
+
+		class R2(WTaskRegistry):
+			__registry_storage__ = WTaskRegistryStorage()
+
+		class T(WTask, metaclass=WRegisteredTask):
+			__registry__ = R2
+
+		assert(R1.registry_storage().count() == 0)
+		R1.add(T)
+		assert(R1.registry_storage().count() == 1)
+		R1.remove(T)
+		assert (R1.registry_storage().count() == 0)
+
+		R1.add(T)
+		R1.add(T)
+		assert (R1.registry_storage().count() == 2)
+		R1.clear()
+		assert (R1.registry_storage().count() == 0)
--- wasp_general/task/registry.py
+++ wasp_general/task/registry.py
@@ -19,9 +19,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with wasp-general.  If not, see <http://www.gnu.org/licenses/>.
 
-# TODO: document the code
-# TODO: write tests for the code
-
 # noinspection PyUnresolvedReferences
 from wasp_general.version import __author__, __version__, __credits__, __license__, __copyright__, __email__
 # noinspection PyUnresolvedReferences
@@ -29,65 +26,167 @@ from wasp_general.version import __status__
 
 from abc import ABCMeta, abstractmethod
 
+from wasp_general.check.verify import verify_subclass, verify_type
 from wasp_general.task.base import WTask
 
 
+class WRegisteredTask(ABCMeta):
+	""" Metaclass for task, that is stored in registry.  Derived class must redefine __registry__ property
+	(and __registry_tag__ depends on registry storage - see
+	:attr:`.WTaskRegistryStorage.__multiple_tasks_per_tag__`)
+	"""
+
+	__registry_tag__ = None
+	""" Tag, that represent task. Depends on registry storage, independent task could be marked the same tag
+	"""
+	__registry__ = None
+	""" Registry, that holds this task. Must be redefined in derived classes.
+	"""
+
+	def __init__(cls, name, bases, namespace):
+		""" Construct new class. Derived class must redefine __registry__ property (and __registry_tag__
+		depends on registry storage - see :attr:`.WTaskRegistryStorage.__multiple_tasks_per_tag__`)
+
+		:param name: as name in type(cls, name, bases, namespace)
+		:param bases: as bases in type(cls, name, bases, namespace)
+		:param namespace: as namespace in type(cls, name, bases, namespace)
+		"""
+		ABCMeta.__init__(cls, name, bases, namespace)
+
+		if cls.__registry__ is None:
+			raise ValueError('__registry__ must be defined')
+
+		if issubclass(cls.__registry__, WTaskRegistry) is False:
+			raise TypeError(
+				"Property '__registry__' of tasks class has invalid type (must be"
+				"WTaskRegistry or its subclass)"
+			)
+
+		if issubclass(cls, WTask) is False:
+			raise TypeError('This class must inherit WTask class')
+
+		cls.__registry__.add(cls)
+
+
 class WTaskRegistryBase(metaclass=ABCMeta):
+	""" Prototype for registry storage. Derived class must redefined following methods
+	"""
 
 	@abstractmethod
 	def add(self, task):
+		""" Add task to storage
+
+		:param task: task to add (WTask class with WRegisteredTask metaclass)
+		:return: None
+		"""
 		raise NotImplementedError('This method is abstract')
 
 	@abstractmethod
 	def remove(self, task):
+		""" Remove task from storage
+
+		:param task: task to remove (WTask class with WRegisteredTask metaclass)
+		:return: None
+		"""
 		raise NotImplementedError('This method is abstract')
 
 	@abstractmethod
 	def clear(self):
+		""" Remove every task from storage
+
+		:return: None
+		"""
 		raise NotImplementedError('This method is abstract')
 
 	@abstractmethod
 	def tasks(self, registry_tag):
+		""" Get tasks from registry by its tag
+
+		:param registry_tag: any hash-able object
+		:return: Return task or list of tasks
+		"""
 		raise NotImplementedError('This method is abstract')
 
 	@abstractmethod
 	def count(self):
+		""" Registered task count
+
+		:return: int
+		"""
 		raise NotImplementedError('This method is abstract')
 
 
 class WTaskRegistryStorage(WTaskRegistryBase):
+	""" Simple registry storage implementation
+	"""
 
 	__multiple_tasks_per_tag__ = True
+	""" Flag that switches storing behaviour. If True - multiple task with the same tag are permitted,
+	otherwise - task with the same tag is treated as error.
+	"""
 
 	def __init__(self):
+		""" Construct new registry storage
+		"""
 		self.__registry = {}
 
-	def add(self, task):
-		registry_tag = task.__registry_tag__
+	@verify_subclass(task_cls=WTask)
+	@verify_type(task_cls=WRegisteredTask)
+	def add(self, task_cls):
+		""" Add task to this storage. Depends on :attr:`.WTaskRegistryStorage.__multiple_tasks_per_tag__`
+		tasks with the same __registry_tag__ can be treated as error.
+
+		:param task_cls: task to add
+		:return: None
+		"""
+		registry_tag = task_cls.__registry_tag__
 		if registry_tag not in self.__registry.keys():
-			self.__registry[registry_tag] = [task]
+			self.__registry[registry_tag] = [task_cls]
 		elif self.__multiple_tasks_per_tag__ is True:
-			self.__registry[registry_tag].append(task)
+			self.__registry[registry_tag].append(task_cls)
 		else:
 			raise RuntimeError('Multiple tasks with same tag appended')
 
-	def remove(self, task):
-		registry_tag = task.__registry_tag__
+	@verify_subclass(task_cls=WTask)
+	@verify_type(task_cls=WRegisteredTask)
+	def remove(self, task_cls):
+		""" Remove task from the storage. If task class are stored multiple times
+		(if :attr:`.WTaskRegistryStorage.__multiple_tasks_per_tag__` is True) - removes all of them.
+
+		:param task_cls: task to remove
+		:return: None
+		"""
+		registry_tag = task_cls.__registry_tag__
 		if registry_tag in self.__registry.keys():
-			self.__registry[registry_tag].remove(task)
+			self.__registry[registry_tag] = \
+				list(filter(lambda x: x != task_cls, self.__registry[registry_tag]))
 			if len(self.__registry[registry_tag]) == 0:
 				self.__registry.pop(registry_tag)
 
 	def clear(self):
+		""" Removes every task from storage
+
+		:return: None
+		"""
 		self.__registry.clear()
 
 	def tasks(self, registry_tag):
+		""" Get tasks from registry by its tag
+
+		:param registry_tag: any hash-able object
+		:return: Return task (if :attr:`.WTaskRegistryStorage.__multiple_tasks_per_tag__` is not True) or \
+		list of tasks
+		"""
 		if registry_tag not in self.__registry.keys():
 			return None
 		tasks = self.__registry[registry_tag]
 		return tasks if self.__multiple_tasks_per_tag__ is True else tasks[0]
 
 	def count(self):
+		""" Registered task count
+
+		:return: int
+		"""
 		result = 0
 		for tasks in self.__registry.values():
 			result += len(tasks)
@@ -95,11 +194,20 @@ class WTaskRegistryStorage(WTaskRegistryBase):
 
 
 class WTaskRegistry:
+	""" Basic task registry. Derived classes must redefine __registry_storage__ property
+	(see :attr:`.WTaskRegistry.__registry_storage__`)
+	"""
 
 	__registry_storage__ = None
+	""" Storage that is used within this registry
+	"""
 
 	@classmethod
 	def registry_storage(cls):
+		""" Get registry storage
+
+		:return: WTaskRegistryBase
+		"""
 		if cls.__registry_storage__ is None:
 			raise ValueError('__registry_storage__ must be defined')
 		if isinstance(cls.__registry_storage__, WTaskRegistryBase) is False:
@@ -108,30 +216,31 @@ class WTaskRegistry:
 		return cls.__registry_storage__
 
 	@classmethod
-	def add(cls, task):
-		cls.registry_storage().add(task)
+	@verify_subclass(task_cls=WTask)
+	@verify_type(task_cls=WRegisteredTask)
+	def add(cls, task_cls):
+		""" Add task class to storage
+
+		:param task_cls: task to add
+		:return: None
+		"""
+		cls.registry_storage().add(task_cls)
 
 	@classmethod
-	def remove(cls, task):
-		cls.registry_storage().remove(task)
+	@verify_subclass(task_cls=WTask)
+	@verify_type(task_cls=WRegisteredTask)
+	def remove(cls, task_cls):
+		""" Remove task class to storage
+
+		:param task_cls: task to remove
+		:return: None
+		"""
+		cls.registry_storage().remove(task_cls)
 
 	@classmethod
 	def clear(cls):
+		""" Remove every task from storage
+
+		:return: None
+		"""
 		cls.registry_storage().clear()
-
-
-class WRegisteredTask(ABCMeta):
-
-	__registry_tag__ = None
-	__registry__ = None
-
-	def __init__(cls, name, bases, dict):
-		ABCMeta.__init__(cls, name, bases, dict)
-
-		if cls.__registry__ is None:
-			raise ValueError('__registry__ must be defined')
-
-		if issubclass(cls.__registry__, WTaskRegistry) is False:
-			raise TypeError("Property '__registry__' of tasks class has invalid type (must be WTaskRegistry or its subclass)")
-
-		cls.__registry__.add(cls)
