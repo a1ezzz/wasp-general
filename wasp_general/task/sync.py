f99d4ace3e7773a595100a43098d8f9838767029
Ildar Gafurov <dev@binblob.com>
2016-09-07 16:55:28 +0300

    wasp_general/task/sync.py: code updated. Documentation and tested added

--- wasp_general/task/sync.py
+++ wasp_general/task/sync.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# wasp_general/task.sync.py
+# wasp_general/task/sync.py
 #
 # Copyright (C) 2016 the wasp-general authors and contributors
 # <see AUTHORS file>
@@ -19,36 +19,27 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with wasp-general.  If not, see <http://www.gnu.org/licenses/>.
 
-# TODO: document the code
-# TODO: write tests for the code
-
 # noinspection PyUnresolvedReferences
 from wasp_general.version import __author__, __version__, __credits__, __license__, __copyright__, __email__
 # noinspection PyUnresolvedReferences
 from wasp_general.version import __status__
 
-from abc import abstractmethod
+from abc import ABCMeta
 
 from wasp_general.task.base import WStoppableTask, WTaskStatus
 
 
-class WSyncTask(WStoppableTask, WTaskStatus):
+class WSyncTask(WStoppableTask, WTaskStatus, metaclass=ABCMeta):
+	""" Main purpose for this class is to separate and to split big bunch of code into smaller stoppable parts
+	"""
 
 	def __init__(self):
 		WStoppableTask.__init__(self)
 		WTaskStatus.__init__(self)
 
-	@abstractmethod
-	def _start(self):
-		raise NotImplementedError('This method is abstract')
-
-	def _stop(self):
-		pass
-
-	def start(self):
-		self._start()
-		self._started(True)
-
 	def stop(self):
-		self._started(False)
-		self._stop()
+		""" Stop this task. This implementation does nothing.
+
+		:return: None
+		"""
+		pass
