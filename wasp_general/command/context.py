8c7515984051d6dc0e8e65de484234696a64d3cf
Ildar Gafurov <dev@binblob.com>
2017-10-10 21:08:05 +0300

    wasp_general/command/context.py: WCommandContextResult was removed due to WCommandResult update. And WCommandContextSet was removed because of WCommandSet update. 'request_context' arguments were renamed to 'command_context'

--- wasp_general/command/context.py
+++ wasp_general/command/context.py
@@ -109,6 +109,27 @@ class WContext(WContextProto):
 	""" :class:`.WContextProto` implementation
 	"""
 
+	class ContextSerializationHelper(WCommandResult.VarSerializationHelper):
+		""" Helper decompose/compose WContext objects with :meth:`.WContext.import_context`,
+		:meth:`.WContext.export_context` methods
+		"""
+
+		def serialize(self, var_value):
+			""" :meth:`.WCommandResult.VarSerializationHelper.serialize` method implementation
+			"""
+			if var_value is None:
+				return None
+			if isinstance(var_value, WContext) is False:
+				raise TypeError('Invalid context value type')
+			return var_value.export_context(var_value)
+
+		def deserialize(self, serialized_value):
+			""" :meth:`.WCommandResult.VarSerializationHelper.deserialize` method implementation
+			"""
+			if serialized_value is None:
+				return None
+			return WContext.import_context(serialized_value)
+
 	@verify_type(context_name=str, context_value=(str, None), linked_context=(WContextProto, None))
 	def __init__(self, context_name, context_value=None, linked_context=None):
 		""" Create new context request
@@ -151,8 +172,7 @@ class WContext(WContextProto):
 	@classmethod
 	@verify_type(context=(tuple, list, None))
 	def import_context(cls, context):
-		""" Import context to corresponding WContextProto object (:meth:`WCommandContextResult.export_context`
-		reverse operation)
+		""" Import context to corresponding WContextProto object (:meth:`WContext.export_context` reverse operation)
 
 		:param context: context to import
 		:return: WContext
@@ -198,29 +218,29 @@ class WCommandContextAdapter(metaclass=ABCMeta):
 		"""
 		return self.__spec
 
-	@verify_type(request_context=(WContextProto, None))
-	def match(self, request_context=None, **command_env):
+	@verify_type(command_context=(WContextProto, None))
+	def match(self, command_context=None, **command_env):
 		""" Check if context request is compatible with adapters specification. True - if compatible,
 		False - otherwise
 
-		:param request_context: context to check
+		:param command_context: context to check
 		:param command_env: command environment
 		:return: bool
 		"""
 		spec = self.specification()
-		if request_context is None and spec is None:
+		if command_context is None and spec is None:
 			return True
-		elif request_context is not None and spec is not None:
-			return request_context == spec
+		elif command_context is not None and spec is not None:
+			return command_context == spec
 		return False
 
 	@abstractmethod
-	@verify_type(command_tokens=str, request_context=(WContextProto, None))
-	def adapt(self, *command_tokens, request_context=None, **command_env):
+	@verify_type(command_tokens=str, command_context=(WContextProto, None))
+	def adapt(self, *command_tokens, command_context=None, **command_env):
 		""" Adapt the given command tokens with this adapter
 
 		:param command_tokens: command tokens to adapt
-		:param request_context: context
+		:param command_context: context
 		:param command_env: command environment
 		:return: list of str
 		"""
@@ -256,31 +276,31 @@ class WCommandContext(WCommandProto):
 		"""
 		return self.__adapter
 
-	@verify_type('paranoid', command_tokens=str, request_context=(WContextProto, None))
-	def match(self, *command_tokens, request_context=None, **command_env):
+	@verify_type('paranoid', command_tokens=str, command_context=(WContextProto, None))
+	def match(self, *command_tokens, command_context=None, **command_env):
 		""" Match command
 
 		:param command_tokens: command tokens to check
-		:param request_context: command context
+		:param command_context: command context
 		:param command_env: command environment
 		:return: bool
 		"""
-		if self.adapter().match(request_context, **command_env) is False:
+		if self.adapter().match(command_context, **command_env) is False:
 			return False
 
-		command_tokens = self.adapter().adapt(*command_tokens, request_context=request_context, **command_env)
-		return self.original_command().match(*command_tokens, request_context=request_context, **command_env)
+		command_tokens = self.adapter().adapt(*command_tokens, command_context=command_context, **command_env)
+		return self.original_command().match(*command_tokens, command_context=command_context, **command_env)
 
-	@verify_type('paranoid', command_tokens=str, request_context=(WContextProto, None))
-	def exec(self, *command_tokens, request_context=None, **command_env):
+	@verify_type('paranoid', command_tokens=str, command_context=(WContextProto, None))
+	def exec(self, *command_tokens, command_context=None, **command_env):
 		""" Execute command
 
 		:param command_tokens: command tokens to execute
-		:param request_context: command context
+		:param command_context: command context
 		:param command_env: command environment
 		:return: WCommandResult
 		"""
-		if self.adapter().match(request_context, **command_env) is False:
+		if self.adapter().match(command_context, **command_env) is False:
 			cmd = WCommandProto.join_tokens(*command_tokens)
 			spec = self.adapter().specification()
 			if spec is not None:
@@ -289,69 +309,5 @@ class WCommandContext(WCommandProto):
 				spec = ','.join(spec)
 			raise RuntimeError('Command mismatch: %s (context: %s)' % (cmd, spec))
 
-		command_tokens = self.adapter().adapt(*command_tokens, request_context=request_context, **command_env)
-		return self.original_command().exec(*command_tokens, request_context=request_context, **command_env)
-
-
-class WCommandContextResult(WCommandResult):
-	""" This is modified version of command result, that can return target context. Can be used for context
-	switching as a result of some command execution
-	"""
-
-	@verify_type('paranoid', output=(str, None))
-	@verify_type(context=(WContextProto, None))
-	def __init__(self, output=None, error=None, context=None):
-		""" Create new result
-
-		:param output: same as output in :meth:`.WCommandResult.__init__`
-		:param error: same as error in :meth:`.WCommandResult.__init__`
-		:param context: context to set
-		"""
-		WCommandResult.__init__(self, output, error)
-		self.context = context
-
-
-class WCommandContextSet(WCommandSet):
-	""" Class that is able to handle context-oriented command execution. Whenever command is searching or
-	is executing - current context is passed as 'request_context' variable. If a command returns
-	:class:`.WCommandContextResult` object, then current context is replaced with the new one.
-	"""
-
-	@verify_type(command_selector=(WCommandSelector, None))
-	def __init__(self, command_selector=None):
-		""" Create new command set
-
-		:param command_selector: command selector to use
-		"""
-		if command_selector is None:
-			command_selector = WCommandPrioritizedSelector()
-		WCommandSet.__init__(self, command_selector=command_selector)
-		self.__context = None
-
-	def context(self):
-		""" Return current context
-
-		:return: WContextProto
-		"""
-		return self.__context
-
-	@verify_type('paranoid', command_str=str)
-	def exec(self, command_str, **command_env):
-		""" Execute command. If command result returns with context (:class:`.WCommandContextResult` object),
-		this context will be set to this object for future use
-
-		:param command_str: command to execute
-		:param command_env: command environment
-		:return: WCommandResult
-		"""
-		command_tokens = WCommandProto.split_command(command_str)
-		command_obj = self.commands().select(*command_tokens, request_context=self.context(), **command_env)
-		if command_obj is None:
-			raise WCommandSet.NoCommandFound('No suitable command found: "%s"' % command_str)
-
-		result = command_obj.exec(*command_tokens, request_context=self.context(), **command_env)
-
-		if isinstance(result, WCommandContextResult) is True:
-			self.__context = result.context
-
-		return result
+		command_tokens = self.adapter().adapt(*command_tokens, command_context=command_context, **command_env)
+		return self.original_command().exec(*command_tokens, command_context=command_context, **command_env)
