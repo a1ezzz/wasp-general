da095af47e2642f6ba86f103a2606ff577917753
Ildar Gafurov <dev@binblob.com>
2017-10-23 10:53:27 +0300

    wasp_general/io.py: BufferedWriters updated, BufferedReaders added

--- wasp_general/io.py
+++ wasp_general/io.py
@@ -35,6 +35,75 @@ from wasp_general.crypto.aes import WAES
 from wasp_general.crypto.hash import WHash
 
 
+class WHashIO:
+
+	def __init__(self, hash_name):
+		self.__hash_name = hash_name
+		self.__hash_obj = WHash.generator(hash_name).new(b'')
+
+	@verify_type(b=bytes)
+	def update_hash(self, b):
+		self.__hash_obj.update(b)
+
+	def hash_name(self):
+		return self.__hash_name
+
+	def hexdigest(self):
+		return self.__hash_obj.hexdigest()
+
+
+class WResponsiveIO:
+
+	class IOTerminated(Exception):
+		pass
+
+	def __init__(self, stop_event):
+		self.__stop_event = stop_event
+
+	def stop_event(self):
+		return self.__stop_event
+
+
+class WIOChainLink:
+
+	def __init__(self, io_cls, *args, **kwargs):
+		self.__io_cls = io_cls
+		self.__args = args
+		self.__kwargs = kwargs
+
+	def io_cls(self):
+		return self.__io_cls
+
+	def io_obj(self, raw):
+		return self.io_cls()(raw, *self.__args, **self.__kwargs)
+
+
+class WIOChain:
+
+	@verify_type(links=WIOChainLink)
+	def __init__(self, last_io_obj, *links):
+		self.__chain = [last_io_obj]
+
+		for link in links:
+			next_io_obj = link.io_obj(last_io_obj)
+			self.__chain.append(next_io_obj)
+			last_io_obj = next_io_obj
+
+	def first_io(self):
+		return self.__chain[-1]
+
+	def instance(self, io_cls):
+		for io_obj in self:
+			if isinstance(io_obj, io_cls) is True:
+				return io_obj
+
+	def __iter__(self):
+		chain = self.__chain.copy()
+		chain.reverse()
+		for io_obj in chain:
+			yield io_obj
+
+
 class WAESWriter(io.BufferedWriter):
 	""" File-like writer with transparent encryption
 	"""
@@ -82,25 +151,18 @@ class WAESWriter(io.BufferedWriter):
 		io.BufferedWriter.flush(self)
 
 
-class WHashCalculationWriter(io.BufferedWriter):
+class WHashCalculationWriter(io.BufferedWriter, WHashIO):
 
 	@verify_type(hash_name=str)
 	def __init__(self, raw, hash_name):
 		io.BufferedWriter.__init__(self, raw)
-		self.__hash_name = hash_name
-		self.__hash_obj = WHash.generator(hash_name).new(b'')
+		WHashIO.__init__(self, hash_name)
 
-	@verify_type(b=(bytes, memoryview))
+	@verify_type('paranoid', b=(bytes, memoryview))
 	def write(self, b):
-		self.__hash_obj.update(bytes(b))
+		self.update_hash(bytes(b))
 		io.BufferedWriter.write(self, b)
 		return len(b)
-
-	def hash_name(self):
-		return self.__hash_name
-
-	def hexdigest(self):
-		return self.__hash_obj.hexdigest()
 
 
 class WThrottlingWriter(io.BufferedWriter):
@@ -145,53 +207,33 @@ class WThrottlingWriter(io.BufferedWriter):
 		return self.__bytes_processed / (finished_at - self.__started_at)
 
 
-class WResponsiveWriter(io.BufferedWriter):
-
-	class WriterTerminated(Exception):
-		pass
+class WResponsiveWriter(io.BufferedWriter, WResponsiveIO):
 
 	def __init__(self, raw, stop_event):
 		io.BufferedWriter.__init__(self, raw)
-		self.__event = stop_event
+		WResponsiveIO.__init__(self, stop_event)
 
 	@verify_type(b=(bytes, memoryview))
 	def write(self, b):
-		if self.__event.is_set():
-			raise WResponsiveWriter.WriterTerminated('Stop event was set')
+		if self.stop_event().is_set():
+			raise WResponsiveIO.IOTerminated('Stop event was set')
 		io.BufferedWriter.write(self, b)
 		return len(b)
 
 
-class WWriterChainLink:
+class WWriterChainLink(WIOChainLink):
 
 	@verify_subclass(writer_cls=io.BufferedWriter)
 	def __init__(self, writer_cls, *args, **kwargs):
-		self.__writer_cls = writer_cls
-		self.__args = args
-		self.__kwargs = kwargs
-
-	def writer(self, raw):
-		return self.__writer_cls(raw, *self.__args, **self.__kwargs)
+		WIOChainLink.__init__(self, writer_cls, *args, **kwargs)
 
 
-class WWriterChain(io.BufferedWriter):
+class WWriterChain(WIOChain, io.BufferedWriter):
 
 	@verify_type(links=WWriterChainLink)
-	def __init__(self, last_link, *links):
-		self.__chain = [last_link]
-
-		for link in links:
-			next_link = link.writer(last_link)
-			self.__chain.append(next_link)
-			last_link = next_link
-
-		io.BufferedWriter.__init__(self, self.__chain[-1])
-
-	def __iter__(self):
-		chain = self.__chain.copy()
-		chain.reverse()
-		for link in chain:
-			yield link
+	def __init__(self, last_io_obj, *links):
+		WIOChain.__init__(self, last_io_obj, *links)
+		io.BufferedWriter.__init__(self, self.first_io())
 
 	def flush(self):
 		io.BufferedWriter.flush(self)
@@ -200,5 +242,83 @@ class WWriterChain(io.BufferedWriter):
 
 	def close(self):
 		io.BufferedWriter.close(self)
+		for link in self:
+			link.close()
+
+
+class WBufferedIOReader(io.BufferedReader):
+
+	def __init__(self, raw):
+		io.BufferedReader.__init__(self, raw)
+
+	def writable(self, *args, **kwargs):
+		return False
+
+	def read(self, size=-1):
+		if size == 0:
+			return self.read_chunk(size)
+
+		result = b''
+		chunk_size = io.DEFAULT_BUFFER_SIZE
+		read_chunk = True
+		while read_chunk is True:
+			next_chunk = self.read_chunk(chunk_size)
+
+			if size > 0:
+				if chunk_size >= size:
+					read_chunk = False
+				size -= chunk_size
+			elif next_chunk == b'':
+				read_chunk = False
+
+			result += next_chunk
+
+		return result
+
+	def read_chunk(self, size):
+		return self.raw.read(size)
+
+
+class WHashCalculationReader(WBufferedIOReader, WHashIO):
+
+	@verify_type(hash_name=str)
+	def __init__(self, raw, hash_name):
+		WBufferedIOReader.__init__(self, raw)
+		WHashIO.__init__(self, hash_name)
+
+	def read_chunk(self, size):
+		result = WBufferedIOReader.read_chunk(self, size)
+		self.update_hash(result)
+		return result
+
+
+class WResponsiveReader(WBufferedIOReader, WResponsiveIO):
+
+	def __init__(self, raw, stop_event):
+		WBufferedIOReader.__init__(self, raw)
+		WResponsiveIO.__init__(self, stop_event)
+
+	def read_chunk(self, size):
+		if self.stop_event().is_set():
+			raise WResponsiveIO.IOTerminated('Stop event was set')
+		return WBufferedIOReader.read_chunk(self, size)
+
+
+class WReaderChainLink(WIOChainLink):
+
+	@verify_subclass(reader_cls=io.BufferedReader)
+	def __init__(self, reader_cls, *args, **kwargs):
+		WIOChainLink.__init__(self, reader_cls, *args, **kwargs)
+
+
+class WReaderChain(WIOChain, io.BufferedReader):
+
+	@verify_type(links=WReaderChainLink)
+	def __init__(self, last_io_obj, *links):
+		WIOChain.__init__(self, last_io_obj, *links)
+		io.BufferedReader.__init__(self, self.first_io())
+
+	def close(self):
+		io.BufferedReader.close(self)
 		for link in self:
 			link.close()
