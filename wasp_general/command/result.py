7f844658ebe494f7f1226b6bba34670a0f9b906b
Ildar Gafurov <dev@binblob.com>
2017-12-18 14:44:11 +0300

    wasp_general/command/result.py: updated

--- wasp_general/command/result.py
+++ wasp_general/command/result.py
@@ -27,11 +27,9 @@ from wasp_general.version import __author__, __version__, __credits__, __license
 # noinspection PyUnresolvedReferences
 from wasp_general.version import __status__
 
-from abc import ABCMeta, abstractmethod
-
-from wasp_general.command.proto import WCommandResultProto
 from wasp_general.verify import verify_type, verify_value
-from wasp_general.cli.formatter import WConsoleTableFormatter
+from wasp_general.template import WTemplate, WTemplateRenderer
+from wasp_general.command.proto import WCommandResultProto
 
 
 # noinspection PyAbstractClass
@@ -55,95 +53,43 @@ class WPlainCommandResult(WCommandEnv):
 	def __str__(self):
 		return self.__result
 
-
-class WPlainErrorCommandResult(WPlainCommandResult):
-
-	def __str__(self):
-		return 'Error. ' + WPlainCommandResult.__str__(self)
+	@classmethod
+	def error(cls, message, **command_env):
+		return WPlainCommandResult('Error. ' + message, **command_env)
 
 
-class WCommandResultEntryProto(metaclass=ABCMeta):
+class WExceptionResult(WCommandEnv):
 
-	@verify_type(entry_tag=(str, None))
-	@verify_value(entry_tag=lambda x: x is None or len(x) > 0)
-	def __init__(self, entry_tag=None):
-		self.__entry_tag = entry_tag
-
-	@abstractmethod
-	def __str__(self):
-		raise NotImplementedError('This method is abstract')
-
-	def entry_tag(self):
-		return self.__entry_tag
-
-
-class WCommandResultEntry(WCommandResultEntryProto):
-
-	@verify_type('paranoid', entry_tag=(str, None))
-	@verify_value('paranoid', entry_tag=lambda x: x is None or len(x) > 0)
-	@verify_type(result=str)
-	def __init__(self, result, entry_tag=None):
-		WCommandResultEntryProto.__init__(self, entry_tag=entry_tag)
-		self.__result = result
-
-	def __str__(self):
-		return self.__result
-
-
-class WDetailedCommandResult(WCommandEnv):
-
-	__default_joining_str__ = '\n'
-
-	@verify_type(entries=(str, WCommandResultEntryProto))
-	def __init__(self, *entries, **command_env):
+	@verify_type(message=str, exception=Exception)
+	def __init__(self, message, exc, traceback, **command_env):
 		WCommandEnv.__init__(self, **command_env)
-		self.__entries = []
-		for entry in entries:
-			if isinstance(entry, str) is True:
-				entry = WCommandResultEntry(entry)
-			self.__entries.append(entry)
+		self.__message = message
+		self.__exception = str(exc)
+		self.__traceback = traceback
 
-	def entries(self):
-		return self.__entries
+	def message(self):
+		return self.__message
 
-	def join_by(self):
-		return self.__default_joining_str__
+	def exception(self):
+		return self.__exception
 
-	def __str__(self):
-		return self.join_by().join([str(x) for x in self.__entries])
-
-
-class WCommandResultTableEntry(WCommandResultEntryProto):
-
-	__default_delimiter__ = '*'
-
-	@verify_type('paranoid', entry_tag=(str, None))
-	@verify_value('paranoid', entry_tag=lambda x: x is None or len(x) > 0)
-	@verify_type(headers=(str, None), delimiter=(str, None))
-	@verify_value(headers=lambda x: x is None or len(x) > 0)
-	@verify_value(delimiter=lambda x: x is None or len(x) == 1)
-	def __init__(self, *headers, delimiter=None, entry_tag=None):
-		WCommandResultEntryProto.__init__(self, entry_tag=entry_tag)
-		self.__headers = headers
-		self.__rows = []
-		self.__delimiter = delimiter if delimiter is not None else self.__default_delimiter__
-
-	def headers(self):
-		return self.__headers
-
-	def rows(self):
-		return self.__rows.copy()
-
-	def delimiter(self):
-		return self.__delimiter
-
-	@verify_type(cells=(str, None))
-	@verify_value(cells=lambda x: x is None or len(x) > 0)
-	def add_row(self, *cells):
-		self.__rows.append(cells)
+	def traceback(self):
+		return self.__traceback
 
 	def __str__(self):
-		table_formatter = WConsoleTableFormatter(*self.headers())
-		for row in self.rows():
-			table_formatter.add_row(*(map(lambda x: x if x is not None else '', row)))
-		return table_formatter.format(self.delimiter())
+		return (
+			'%s\nException was raised. %s\n%s' %
+			(self.message(), self.exception(), self.traceback())
+		)
+
+
+class WCommandResultTemplate(WPlainCommandResult, WTemplateRenderer):
+
+	@verify_type('paranoid', template=WTemplate)
+	def __init__(self, template, **command_env):
+		WPlainCommandResult.__init__(self, '', **command_env)
+		WTemplateRenderer.__init__(self, template)
+		self.__template = template
+
+	def __str__(self):
+		return self.render()
