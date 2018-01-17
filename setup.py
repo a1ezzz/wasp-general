f66ce26161560b480d82e9c54be90ab3f2a954aa
Ildar Gafurov <dev@binblob.com>
2018-01-17 12:23:37 +0300

    build bug fix

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ def read(fname):
 	return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
+__pypi_data__ = __package_data__['pypi']
+
+
 if __name__ == "__main__":
 	setup(
 		name=__package_data__['package'],
@@ -19,15 +22,15 @@ if __name__ == "__main__":
 		maintainer_email=__package_data__['maintainer_email'],
 		description=__package_data__['brief_description'],
 		license=__license__,
-		keywords=__package_data__['pypi']['keywords'],
+		keywords=__pypi_data__['keywords'],
 		url=__package_data__['homepage'],
 		packages=find_packages(),
-		include_package_data=True,
+		include_package_data=\
+			__pypi_data__['include_package_data'] if 'include_package_data' in __pypi_data__ else True,
 		long_description=read(__package_data__['readme_file']),
-		classifiers=__package_data__['pypi']['classifiers'],
-		install_requires=read(__package_data__['requirements.txt']).splitlines(),
-		zip_safe=__package_data__['pypi']['zip_safe'] if 'zip_safe' in __package_data__['pypi'] else False,
+		classifiers=__pypi_data__['classifiers'],
+		install_requires=read('requirements.txt').splitlines(),
+		zip_safe=__pypi_data__['zip_safe'] if 'zip_safe' in __pypi_data__ else False,
 		scripts=__package_data__['scripts'] if 'scripts' in __package_data__ else [],
-		extras_require= \
-			__package_data__['pypi']['extra_require'] if 'extra_require' in __package_data__['pypi'] else {}
+		extras_require=__pypi_data__['extra_require'] if 'extra_require' in __pypi_data__ else {}
 	)
