c1dcba16bb8454d43eb859b536cbb3a6ada709e0
Ildar Gafurov <dev@binblob.com>
2018-05-20 06:57:52 +0300

    tests/conftest.py: temp_dir fixture was added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,27 @@
 
-from tempfile import mktemp
+from tempfile import mktemp, mkdtemp
 import os
 import pytest
+import shutil
 
 
 @pytest.fixture
 def temp_file(request):
-	filename = mktemp('pytest-wasp-general-')
+	filename = mktemp('-pytest-wasp-general')
 
 	def fin():
 		if os.path.exists(filename):
 			os.unlink(filename)
 	request.addfinalizer(fin)
 	return filename
+
+
+@pytest.fixture
+def temp_dir(request):
+	dir_name = mkdtemp('-pytest-wasp-general')
+
+	def fin():
+		if os.path.exists(dir_name):
+			shutil.rmtree(dir_name)
+	request.addfinalizer(fin)
+	return dir_name
