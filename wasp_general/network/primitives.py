73acd7151d93282a419baafa60ab2ff1e6764b31
Ildar Gafurov <dev@binblob.com>
2019-03-06 20:23:07 +0300

    wasp_general/network/primitives.py: docs, test and smal fixes

--- wasp_general/network/primitives.py
+++ wasp_general/network/primitives.py
@@ -64,6 +64,7 @@ class WMACAddress:
 		""" Construct new address
 
 		:param address: value with which this address is initialized (zeroes are used by default).
+		:type address: str | WBinArray | int | None
 		"""
 		self.__address = WFixedSizeByteArray(WMACAddress.octet_count)
 
@@ -78,17 +79,19 @@ class WMACAddress:
 	def bin_address(self):
 		""" Get this address as sequence of bits
 
-		:return: WBinArray
+		:rtype: WBinArray
 		"""
 		return self.__address.bin_value()
 
 	@staticmethod
-	@verify_type(address=str)
+	@verify_type('strict', address=str)
 	def from_string(address):
 		""" Return new object by the given MAC-address
 
 		:param address: address to convert
-		:return: WMACAddress
+		:type address: str
+
+		:rtype: WMACAddress
 		"""
 		str_address = None
 
@@ -115,7 +118,7 @@ class WMACAddress:
 	def __str__(self):
 		""" Convert to string
 
-		:return: str
+		:rtype: str
 		"""
 		address = ["{:02x}".format(int(x)) for x in self.__address]
 		return ':'.join(address)
@@ -123,7 +126,7 @@ class WMACAddress:
 	def __bytes__(self):
 		""" Convert to bytes
 
-		:return: bytes
+		:rtype: bytes
 		"""
 		return bytes(self.__address)
 
@@ -143,6 +146,7 @@ class WIPV4Address:
 		""" Create new address
 
 		:param address: value with which this address is initialized (zeroes are used by default).
+		:type address: str | bytes | WBinArray | int | None
 		"""
 		self.__address = WFixedSizeByteArray(WIPV4Address.octet_count)
 
@@ -157,24 +161,26 @@ class WIPV4Address:
 	def bin_address(self):
 		""" Convert address to WBinArray
 
-		:return: WBinArray
+		:rtype: WBinArray
 		"""
 		return self.__address.bin_value()
 
 	def __bytes__(self):
 		""" Convert address to bytes
 
-		:return: bytes
+		:rtype: bytes
 		"""
 		return bytes(self.__address)
 
 	@staticmethod
-	@verify_type(address=str)
+	@verify_type('strict', address=str)
 	def from_string(address):
 		""" Parse string for IPv4 address
 
 		:param address: address to parse
-		:return:
+		:type address: str
+
+		:rtype: WIPV4Address
 		"""
 		address = address.split('.')
 		if len(address) != WIPV4Address.octet_count:
@@ -185,26 +191,35 @@ class WIPV4Address:
 			result.__address[i] = WBinArray(int(address[i]), WFixedSizeByteArray.byte_size)
 		return result
 
-	@staticmethod
-	@verify_type(dns_format=bool)
-	def to_string(address, dns_format=False):
+	@verify_type('strict', dns_format=bool)
+	def to_string(self, dns_format=False):
 		""" Convert address to string
 
-		:param address: WIPV4Address to convert
 		:param dns_format: whether to use arpa-format or not
-		:return:
-		"""
-		if isinstance(address, WIPV4Address) is False:
-			raise TypeError('Invalid address type')
+		:type dns_format: bool
 
-		address = [str(int(x)) for x in address.__address]
+		:rtype: str
+		"""
+		address = [str(int(x)) for x in self.__address]
 		if dns_format is False:
 			return '.'.join(address)
 		address.reverse()
-		return ('.'.join(address) + '.in-addr.arpa')
+		return '.'.join(address) + '.in-addr.arpa'
 
 	def __str__(self):
+		""" Convert this address to string
+
+		:rtype: str
+		"""
 		return WIPV4Address.to_string(self)
+
+	@classmethod
+	def bits_count(cls):
+		""" Return number of bits IPv4 address consists of
+
+		:rtype: int
+		"""
+		return WFixedSizeByteArray.byte_size * WIPV4Address.octet_count
 
 
 class WNetworkIPV4:
@@ -218,37 +233,34 @@ class WNetworkIPV4:
 	""" Separator that separate network address from network mask.
 	"""
 
-	@verify_type(address=(str, tuple, list, set), network_address=bool)
-	@verify_value(address=lambda x: isinstance(x, (tuple, list, set)) is False or len(x) == 2)
-	@verify_value(address=lambda x: isinstance(x, (tuple, list, set)) is False or isinstance(x[0], WIPV4Address))
-	@verify_value(address=lambda x: isinstance(x, (tuple, list, set)) is False or isinstance(x[1], int))
-	def __init__(self, address, network_address=True):
+	@verify_type('strict', ipv4_address=(WIPV4Address, str, bytes, WBinArray, int), network_mask=int)
+	@verify_type('strict', network_address=bool)
+	@verify_value('strict', network_mask=lambda x: 0 <= x <= WIPV4Address.bits_count())
+	def __init__(self, ipv4_address, network_mask, network_address=True):
 		""" Construct new network address
 
-		:param address: address as a string or as tuple/list/set. where the first element is WIPV4Address and \
-		the second one is network mask (as bits count)
+		:param ipv4_address: IPv4 address or object that may be casted to
+		:type ipv4_address: WIPV4Address | str | bytes | WBinArray | int
+
+		:param network_mask: network mask (number of bits)
+		:type network_mask: int
+
 		:param network_address: whether this object is host address with network mask or an address of a network
+		:type network_address: bool
 		"""
 
-		if isinstance(address, str) is True:
-			ip_address, self.__mask = address.strip().split(WNetworkIPV4.delimiter)
-			self.__address = WIPV4Address(ip_address)
-		else:
-			self.__address = address[0]
-			self.__mask = address[1]
-
-		self.__mask = int(self.__mask)
-		if self.__mask < 0 or self.__mask > len(self.__address.bin_address()):
-			raise ValueError(
-				'Invalid network mask: %s (network specified: %s)' % (str(self.__mask), address)
-			)
+		if isinstance(ipv4_address, WIPV4Address) is False:
+			ipv4_address = WIPV4Address(ipv4_address)
+		self.__address = ipv4_address
+		self.__mask = network_mask
 
 		self.__network_address__ = network_address
 		if network_address:
 			bin_address = self.__address.bin_address()
 			if int(bin_address[self.__mask:]) != 0:
 				raise ValueError(
-					'Invalid network mask: %s (network specified: %s)' % (str(self.__mask), address)
+					'Invalid network mask: %s (network specified: %s)' %
+					(str(self.__mask), ipv4_address)
 				)
 
 	def address(self):
@@ -265,13 +277,14 @@ class WNetworkIPV4:
 		"""
 		return self.__mask
 
-	@verify_type(skip_network_address=bool)
+	@verify_type('strict', skip_network_address=bool)
 	def first_address(self, skip_network_address=True):
 		""" Return the first IP address of this network
 
 		:param skip_network_address: this flag specifies whether this function returns address of the network \
 		or returns address that follows address of the network (address, that a host could have)
-		:return: WIPV4Address
+
+		:rtype: WIPV4Address
 		"""
 		bin_address = self.__address.bin_address()
 		bin_address_length = len(bin_address)
@@ -286,13 +299,14 @@ class WNetworkIPV4:
 
 		return WIPV4Address(bin_address)
 
-	@verify_type(skip_broadcast_address=bool)
+	@verify_type('strict', skip_broadcast_address=bool)
 	def last_address(self, skip_broadcast_address=True):
 		""" Return the last IP address of this network
 
 		:param skip_broadcast_address: this flag specifies whether to skip the very last address (that is \
 		usually used as broadcast address) or not.
-		:return: WIPV4Address
+
+		:rtype: WIPV4Address
 		"""
 		bin_address = self.__address.bin_address()
 		bin_address_length = len(bin_address)
@@ -307,56 +321,96 @@ class WNetworkIPV4:
 
 		return WIPV4Address(bin_address)
 
+	@verify_type('strict', skip_network_address=bool, skip_broadcast_address=bool)
 	def iterator(self, skip_network_address=True, skip_broadcast_address=True):
 		""" Return iterator, that can iterate over network addresses
 
 		:param skip_network_address: same as skip_network_address in :meth:`.NetworkIPV4.first_address` method
+		:type skip_network_address: bool
+
 		:param skip_broadcast_address: same as skip_broadcast_address in :meth:`.NetworkIPV4.last_address` \
 		method
-		:return: NetworkIPV4Iterator
+		:type skip_broadcast_address: bool
+
+		:rtype: NetworkIPV4Iterator
 		"""
 		return WNetworkIPV4Iterator(self, skip_network_address, skip_broadcast_address)
 
-	@verify_type(address=WIPV4Address)
+	@verify_type('strict', address=WIPV4Address)
 	def __contains__(self, address):
 		""" Check if this network contains specified IP address.
 
 		:param address: address to check
-		:return: bool
+		:type address: WIPV4Address
+
+		:rtype: bool
 		"""
 		int_value = int(address.bin_address())
 		first_address = int(self.first_address().bin_address())
 		last_address = int(self.last_address().bin_address())
-		return int_value >= first_address and int_value <= last_address
+		return first_address <= int_value <= last_address
 
 	@staticmethod
-	@verify_type(address=WIPV4Address)
+	@verify_type('strict', address=WIPV4Address)
 	def is_multicast(address):
 		""" Check if address is a multicast address.
 
 		:param address: IP address to check
-		:return: bool
+		:type address: WIPV4Address
+
+		:rtype: bool
 
 		see also https://tools.ietf.org/html/rfc5771
 		"""
-		return address in WNetworkIPV4('224.0.0.0/4')
+		return address in WNetworkIPV4.from_string('224.0.0.0/4')
+
+	@staticmethod
+	@verify_type('strict', address=str)
+	@verify_type('paranoid', network_address=bool)
+	def from_string(address, network_address=True):
+		""" Parse string for IPv4 address
+
+		:param address: address to parse
+		:type address: str
+
+		:param network_address: same as network_address in :meth:`.WNetworkIPV4.__init__` - whether this \
+		object is host address with network mask or an address of a network
+		:type network_address: bool
+
+		:rtype: WNetworkIPV4
+		"""
+
+		address, mask = address.strip().split(WNetworkIPV4.delimiter)
+		return WNetworkIPV4(address, int(mask), network_address=network_address)
+
+	def __str__(self):
+		""" Convert this network address to string
+
+		:rtype: str
+		"""
+		return str(self.__address) + WNetworkIPV4.delimiter + str(self.__mask)
 
 
 class WNetworkIPV4Iterator:
 	""" This iterator iterates over IP network addresses.
 	"""
 
-	@verify_type(network=(WIPV4Address, WNetworkIPV4), skip_network_address=bool, skip_broadcast_address=bool)
+	@verify_type('strict', network=(WIPV4Address, WNetworkIPV4), skip_network_address=bool, skip_broadcast_address=bool)
 	def __init__(self, network, skip_network_address=True, skip_broadcast_address=True):
 		""" Create new iterator
 
 		:param network: network to iterate. If it is WIPV4Address instance, then iterate over single address
+		:type network: WIPV4Address | WNetworkIPV4
+
 		:param skip_network_address: same as skip_network_address in :meth:`.NetworkIPV4.first_address` method
+		:type skip_network_address: bool
+
 		:param skip_broadcast_address: same as skip_broadcast_address in :meth:`.NetworkIPV4.last_address` \
 		method
+		:type skip_broadcast_address: bool
 		"""
 		if isinstance(network, WIPV4Address) is True:
-			self.__network = WNetworkIPV4((network, len(network.bin_address())))
+			self.__network = WNetworkIPV4(network, len(network.bin_address()))
 		else:
 			# isinstance(network, NetworkIPV4) is True
 			self.__network = network
@@ -367,7 +421,7 @@ class WNetworkIPV4Iterator:
 	def __iter__(self):
 		""" Iterate call
 
-		:return: None
+		:rtype: generator
 		"""
 		first_address = self.__network.first_address(skip_network_address=self.__skip_network_address)
 		last_address = self.__network.last_address(skip_broadcast_address=self.__skip_broadcast_address)
@@ -391,24 +445,25 @@ class WIPPort:
 	""" Maximum port number
 	"""
 
-	@verify_type(port=int)
-	@verify_value(port=lambda x: WIPPort.minimum_port_number <= x <= WIPPort.maximum_port_number)
+	@verify_type('strict', port=int)
+	@verify_value('strict', port=lambda x: WIPPort.minimum_port_number <= x <= WIPPort.maximum_port_number)
 	def __init__(self, port):
 		""" Construct new IP port
 
 		:param port: initialization value
+		:type port: int
 		"""
 		self.__port = port
 
 	def __int__(self):
 		""" Return port number as int
-		:return: int
+		:rtype: int
 		"""
 		return self.__port
 
 	def __str__(self):
 		""" Return port number as string
-		:return: str
+		:rtype: str
 		"""
 		return str(self.__port)
 
@@ -419,32 +474,35 @@ class WFQDN:
 	see also https://en.wikipedia.org/wiki/Fully_qualified_domain_name
 	"""
 
-	re_label = re.compile('^[a-zA-Z0-9\-]{1,63}$')
+	re_label = re.compile('^[a-zA-Z0-9-]{1,63}$')
 	""" Regular expression for FQDN label (sequence between dots) as is specified in ....
 	"""
 
 	maximum_fqdn_length = 253
 
-	@verify_type(address=(str, list, tuple, set, None))
+	@verify_type('strict', address=(str, list, tuple, set, None))
 	def __init__(self, address=None):
 		""" Construct new FQDN. If no address is specified, then this FQDN represent root node, which is '.'
 
 		:param address: FQDN address in string or in list/tuple/set of labels
+		:type address: str | (str) | list[str]
 		"""
-		self._labels = []
+		self.__labels = []
 
 		if isinstance(address, str) is True:
-			self._labels = WFQDN.from_string(address)._labels
+			self.__labels = WFQDN.from_string(address).__labels
 		elif isinstance(address, (list, tuple, set)) is True:
-			self._labels = WFQDN.from_string('.'.join(address))._labels
+			self.__labels = WFQDN.from_string('.'.join(address)).__labels
 
 	@staticmethod
-	@verify_type(address=str)
+	@verify_type('strict', address=str)
 	def from_string(address):
 		""" Convert doted-written FQDN address to WFQDN object
 
 		:param address: address to convert
-		:return: WFQDN
+		:type address: str
+
+		:rtype: WFQDN
 		"""
 		if len(address) == 0:
 			return WFQDN()
@@ -458,42 +516,41 @@ class WFQDN:
 		result = WFQDN()
 		for label in address.split('.'):
 			if isinstance(label, str) and WFQDN.re_label.match(label):
-				result._labels.append(label)
+				result.__labels.append(label)
 			else:
 				raise ValueError('Invalid address')
 
 		return result
 
-	@staticmethod
-	@verify_type(leading_dot=bool)
-	def to_string(address, leading_dot=False):
+	@verify_type('strict', leading_dot=bool)
+	def to_string(self, leading_dot=False):
 		""" Return doted-written address by the given WFQDN object
 
-		:param address: address to convert
 		:param leading_dot: whether this function place leading dot to the result or not
-		:return: str
+		:type leading_dot: bool
+
+		:rtype: str
 		"""
 
-		if isinstance(address, WFQDN) is False:
-			raise TypeError('Invalid type for FQDN address')
-
-		result = '.'.join(address._labels)
+		result = '.'.join(self.__labels)
 		return result if leading_dot is False else (result + '.')
 
 	def __str__(self):
 		""" Return string
 
-		:return: str
+		:rtype: str
 		"""
-		return WFQDN.to_string(self)
+		return self.to_string()
 
 	@staticmethod
-	@verify_type(idn_fqdn=str)
+	@verify_type('strict', idn_fqdn=str)
 	def punycode(idn_fqdn):
 		""" Create WFQDN from IDN (Internationalized domain name) by reverting it to punycode
 
 		:param idn_fqdn: internationalized domain name to convert
-		:return: WFQDN
+		:type idn_fqdn: str
+
+		:rtype: WFQDN
 
 		see also https://en.wikipedia.org/wiki/Internationalized_domain_name
 		see also https://en.wikipedia.org/wiki/Punycode
@@ -508,12 +565,19 @@ class WIPV4SocketInfo:
 	see :meth:`.WIPV4SocketInfo.pair`
 	"""
 
+	delimiter = ':'
+	""" Separator that separate address from port number
+	"""
+
 	@verify_type('paranoid', address=(WFQDN, WIPV4Address, str, WBinArray, int, None), port=(WIPPort, int, None))
 	def __init__(self, address=None, port=None):
 		""" Construct new pair
 
 		:param address: associated IP address
+		:type address: WFQDN | WIPV4Address | str | WBinArray | int | None
+
 		:param port: associated IP port
+		:type port: WIPPort | int | None
 		"""
 		self.__address = None
 		if address is not None:
@@ -531,14 +595,14 @@ class WIPV4SocketInfo:
 	def address(self):
 		""" Return associated IP address or None if not available
 
-		:return: WIPV4Address or WFQDN or None
+		:rtype: WIPV4Address | WFQDN | None
 		"""
 		return self.__address
 
 	def port(self):
 		""" Return associated IP port or None if not available
 
-		:return: WIPPort or None
+		:rtype: WIPPort | None
 		"""
 		return self.__port
 
@@ -547,20 +611,22 @@ class WIPV4SocketInfo:
 		port is an integer (zero if self.port() is None). Mainly, this tuple is used with python socket module
 		(like in bind method)
 
-		:return: 2 value tuple of str and int.
+		:rtype: (str, int)
 		"""
 		address = str(self.__address) if self.__address is not None else ''
 		port = int(self.__port) if self.__port is not None else 0
 		return address, port
 
 	@staticmethod
-	@verify_type(address=str)
+	@verify_type('strict', address=str)
 	def parse_address(address):
 		""" Parse string and return :class:`.WIPV4Address` object if an IP address is specified,
 		:class:`.WFQDN` if domain name is specified and None if the string is empty.
 
 		:param address: string to parse
-		:return: WIPV4Address or WFQDN or None
+		:type address: str
+
+		:rtype: WIPV4Address | WFQDN | None
 		"""
 
 		if len(address) == 0:
@@ -579,17 +645,18 @@ class WIPV4SocketInfo:
 		raise ValueError('Unable to parse address string. It must be an IP address or FQDN')
 
 	@classmethod
-	@verify_type(info=str)
-	@verify_value(info=lambda x: len(x) > 0)
+	@verify_type('strict', info=str)
+	@verify_value('strict', info=lambda x: len(x) > 0)
 	def parse_socket_info(cls, info):
 		""" Parse string that is formed like '[address]<:port>' and return corresponding
 		:class:`.WIPV4ScketInfo` object
 
 		:param info: string to parse
+		:type info: str
 
-		:return: WIPV4ScketInfo
+		:rtype: WIPV4SocketInfo
 		"""
-		info = info.split(':')
+		info = info.split(cls.delimiter)
 		if len(info) > 2:
 			raise ValueError('Incorrect socket info specified')
 		address = info[0].strip()
