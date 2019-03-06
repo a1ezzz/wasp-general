51cd3b3d9661003cb8c487048f5ebf5df67628c3
Ildar Gafurov <dev@binblob.com>
2019-03-06 20:23:07 +0300

    wasp_general/network/primitives.py: docs, test and smal fixes

--- tests/wasp_general_network_primitives_test.py
+++ tests/wasp_general_network_primitives_test.py
@@ -66,7 +66,6 @@ class TestWIPV4Address:
 		assert(str(WIPV4Address.from_string("10.192.0.1").bin_address()) == "00001010110000000000000000000001")
 
 	def test_to_string(self):
-		pytest.raises(TypeError, WIPV4Address.to_string, '10.192.0.1')
 		address = WIPV4Address(WBinArray(0b00001010110000000000000000000001))
 		assert(WIPV4Address.to_string(address) == "10.192.0.1")
 		assert(WIPV4Address.to_string(address, dns_format=True) == "1.0.192.10.in-addr.arpa")
@@ -78,73 +77,77 @@ class TestWIPV4Address:
 		assert(bytes(WIPV4Address(WBinArray(0b00001010110000000000000000000001))) == b"\x0a\xc0\x00\x01")
 
 
-class TestNetworkIPV4:
+class TestWNetworkIPV4:
 
 	def test_init(self):
-		assert(isinstance(WNetworkIPV4('10.0.0.0/24'), WNetworkIPV4) is True)
-		pytest.raises(ValueError, WNetworkIPV4, '10.0.0.0/33')
-		assert(isinstance(WNetworkIPV4('0.0.0.0/0'), WNetworkIPV4) is True)
-		pytest.raises(ValueError, WNetworkIPV4, '10.0.0.0/-1')
-		pytest.raises(ValueError, WNetworkIPV4, '10.0.0.1/24')
-		assert(isinstance(WNetworkIPV4('10.0.0.1/24', network_address=False), WNetworkIPV4) is True)
-		assert(isinstance(WNetworkIPV4((WIPV4Address('192.168.0.0'), 24)), WNetworkIPV4) is True)
+		assert(isinstance(WNetworkIPV4(WIPV4Address('192.168.0.0'), 24), WNetworkIPV4) is True)
+		assert(isinstance(WNetworkIPV4('192.168.0.0', 24), WNetworkIPV4) is True)
+		assert(isinstance(WNetworkIPV4.from_string('10.0.0.0/24'), WNetworkIPV4) is True)
+		pytest.raises(ValueError, WNetworkIPV4, '10.0.0.0', 33)
+		assert(isinstance(WNetworkIPV4.from_string('0.0.0.0/0'), WNetworkIPV4) is True)
+		pytest.raises(ValueError, WNetworkIPV4.from_string, '10.0.0.0/-1')
+		pytest.raises(ValueError, WNetworkIPV4.from_string, '10.0.0.1/24')
+		assert(isinstance(WNetworkIPV4.from_string('10.0.0.1/24', network_address=False), WNetworkIPV4) is True)
+
+		assert(str(WNetworkIPV4.from_string('10.0.0.0/28')) == '10.0.0.0/28')
+		assert(str(WNetworkIPV4.from_string('10.0.0.1/20', network_address=False)) == '10.0.0.1/20')
 
 	def test_address(self):
-		assert(isinstance(WNetworkIPV4('10.0.0.0/24').address(), WIPV4Address) is True)
-		assert(int(WNetworkIPV4('0.0.1.0/24').address().bin_address()) == 256)
-		assert(int(WNetworkIPV4((WIPV4Address('0.0.1.0'), 24)).address().bin_address()) == 256)
+		assert(isinstance(WNetworkIPV4.from_string('10.0.0.0/24').address(), WIPV4Address) is True)
+		assert(int(WNetworkIPV4.from_string('0.0.1.0/24').address().bin_address()) == 256)
+		assert(int(WNetworkIPV4(WIPV4Address('0.0.1.0'), 24).address().bin_address()) == 256)
 
 	def test_mask(self):
-		assert(isinstance(WNetworkIPV4('10.0.0.0/24').mask(), int) is True)
-		assert(int(WNetworkIPV4('0.0.1.0/24').mask()) == 24)
+		assert(isinstance(WNetworkIPV4.from_string('10.0.0.0/24').mask(), int) is True)
+		assert(int(WNetworkIPV4.from_string('0.0.1.0/24').mask()) == 24)
 
 	def test_first_address(self):
-		assert(str(WNetworkIPV4('10.0.0.0/24').first_address()) == '10.0.0.1')
-		assert(str(WNetworkIPV4('10.0.0.0/24').first_address(skip_network_address=False)) == '10.0.0.0')
-		assert(str(WNetworkIPV4('10.0.0.1/24', network_address=False).first_address()) == '10.0.0.1')
-		net = WNetworkIPV4('10.0.0.1/24', network_address=False)
+		assert(str(WNetworkIPV4.from_string('10.0.0.0/24').first_address()) == '10.0.0.1')
+		assert(str(WNetworkIPV4('10.0.0.0', 24).first_address(skip_network_address=False)) == '10.0.0.0')
+		assert(str(WNetworkIPV4('10.0.0.1', 24, network_address=False).first_address()) == '10.0.0.1')
+		net = WNetworkIPV4.from_string('10.0.0.1/24', network_address=False)
 		assert(str(net.first_address(skip_network_address=False)) == '10.0.0.0')
 
 	def test_last_address(self):
-		assert(str(WNetworkIPV4('10.0.0.0/23').last_address()) == '10.0.1.254')
-		assert(str(WNetworkIPV4('10.0.0.0/23').last_address(skip_broadcast_address=False)) == '10.0.1.255')
-		assert(str(WNetworkIPV4('10.0.0.1/23', network_address=False).last_address()) == '10.0.1.254')
-		net = WNetworkIPV4('10.0.0.1/23', network_address=False)
+		assert(str(WNetworkIPV4.from_string('10.0.0.0/23').last_address()) == '10.0.1.254')
+		assert(str(WNetworkIPV4('10.0.0.0', 23).last_address(skip_broadcast_address=False)) == '10.0.1.255')
+		assert(str(WNetworkIPV4('10.0.0.1', 23, network_address=False).last_address()) == '10.0.1.254')
+		net = WNetworkIPV4.from_string('10.0.0.1/23', network_address=False)
 		assert(str(net.last_address(skip_broadcast_address=False)) == '10.0.1.255')
 
 	def test_iterator(self):
-		assert(len(list(WNetworkIPV4('10.0.0.0/32').iterator())) == 1)
-		assert(str(list(WNetworkIPV4('10.0.0.0/32').iterator())[0]) == '10.0.0.0')
-		assert(len(list(WNetworkIPV4('10.0.0.0/32').iterator(skip_network_address=False))) == 1)
-		assert(len(list(WNetworkIPV4('10.0.0.0/32').iterator(skip_broadcast_address=False))) == 1)
-		net = WNetworkIPV4('10.0.0.0/32')
+		assert(len(list(WNetworkIPV4.from_string('10.0.0.0/32').iterator())) == 1)
+		assert(str(list(WNetworkIPV4.from_string('10.0.0.0/32').iterator())[0]) == '10.0.0.0')
+		assert(len(list(WNetworkIPV4('10.0.0.0', 32).iterator(skip_network_address=False))) == 1)
+		assert(len(list(WNetworkIPV4('10.0.0.0', 32).iterator(skip_broadcast_address=False))) == 1)
+		net = WNetworkIPV4.from_string('10.0.0.0/32')
 		assert(len(list(net.iterator(skip_broadcast_address=False, skip_network_address=False))) == 1)
 
-		assert(len(list(WNetworkIPV4('10.0.0.0/31').iterator())) == 2)
-		assert(str(list(WNetworkIPV4('10.0.0.0/31').iterator())[0]) == '10.0.0.0')
-		assert(str(list(WNetworkIPV4('10.0.0.0/31').iterator())[1]) == '10.0.0.1')
-		assert(len(list(WNetworkIPV4('10.0.0.0/31').iterator(skip_network_address=False))) == 2)
-		assert(len(list(WNetworkIPV4('10.0.0.0/31').iterator(skip_broadcast_address=False))) == 2)
-		net = WNetworkIPV4('10.0.0.0/31')
+		assert(len(list(WNetworkIPV4.from_string('10.0.0.0/31').iterator())) == 2)
+		assert(str(list(WNetworkIPV4.from_string('10.0.0.0/31').iterator())[0]) == '10.0.0.0')
+		assert(str(list(WNetworkIPV4.from_string('10.0.0.0/31').iterator())[1]) == '10.0.0.1')
+		assert(len(list(WNetworkIPV4.from_string('10.0.0.0/31').iterator(skip_network_address=False))) == 2)
+		assert(len(list(WNetworkIPV4.from_string('10.0.0.0/31').iterator(skip_broadcast_address=False))) == 2)
+		net = WNetworkIPV4.from_string('10.0.0.0/31')
 		assert(len(list(net.iterator(skip_broadcast_address=False, skip_network_address=False))) == 2)
 
-		assert(len(list(WNetworkIPV4('10.0.0.0/28').iterator())) == 14)
-		assert(str(list(WNetworkIPV4('10.0.0.0/28').iterator())[0]) == '10.0.0.1')
-		assert(str(list(WNetworkIPV4('10.0.0.0/28').iterator(skip_network_address=False))[0]) == '10.0.0.0')
-		assert(str(list(WNetworkIPV4('10.0.0.0/28').iterator())[13]) == '10.0.0.14')
-		assert(str(list(WNetworkIPV4('10.0.0.0/28').iterator(skip_broadcast_address=False))[14]) == '10.0.0.15')
-		assert(len(list(WNetworkIPV4('10.0.0.0/28').iterator(skip_network_address=False))) == 15)
-		assert(len(list(WNetworkIPV4('10.0.0.0/28').iterator(skip_broadcast_address=False))) == 15)
-		net = WNetworkIPV4('10.0.0.0/28')
+		assert(len(list(WNetworkIPV4.from_string('10.0.0.0/28').iterator())) == 14)
+		assert(str(list(WNetworkIPV4.from_string('10.0.0.0/28').iterator())[0]) == '10.0.0.1')
+		assert(str(list(WNetworkIPV4.from_string('10.0.0.0/28').iterator(skip_network_address=False))[0]) == '10.0.0.0')
+		assert(str(list(WNetworkIPV4.from_string('10.0.0.0/28').iterator())[13]) == '10.0.0.14')
+		assert(str(list(WNetworkIPV4.from_string('10.0.0.0/28').iterator(skip_broadcast_address=False))[14]) == '10.0.0.15')
+		assert(len(list(WNetworkIPV4.from_string('10.0.0.0/28').iterator(skip_network_address=False))) == 15)
+		assert(len(list(WNetworkIPV4.from_string('10.0.0.0/28').iterator(skip_broadcast_address=False))) == 15)
+		net = WNetworkIPV4.from_string('10.0.0.0/28')
 		assert(len(list(net.iterator(skip_broadcast_address=False, skip_network_address=False))) == 16)
 
 		it = WNetworkIPV4Iterator(WIPV4Address('10.0.0.0'))
 		assert(len(list(it)) == 1)
 
 	def test_contains(self):
-		assert((WIPV4Address('192.168.0.1') in WNetworkIPV4('192.168.0.0/24')) is True)
-		assert((WIPV4Address('192.168.0.1') in WNetworkIPV4('192.0.0.0/24')) is False)
-		assert((WIPV4Address('192.168.0.0') in WNetworkIPV4('192.0.0.0/24')) is False)
+		assert((WIPV4Address('192.168.0.1') in WNetworkIPV4.from_string('192.168.0.0/24')) is True)
+		assert((WIPV4Address('192.168.0.1') in WNetworkIPV4.from_string('192.0.0.0/24')) is False)
+		assert((WIPV4Address('192.168.0.0') in WNetworkIPV4.from_string('192.0.0.0/24')) is False)
 
 	def test_multicast(self):
 		assert(WNetworkIPV4.is_multicast(WIPV4Address('192.168.0.1')) is False)
@@ -160,9 +163,8 @@ class TestWFQDN:
 		pytest.raises(ValueError, WFQDN, 'a' * 1000)
 		pytest.raises(ValueError, WFQDN, '\ud13f')
 
-		assert(WFQDN.to_string(WFQDN('site1.org2.fld'), leading_dot=True) == 'site1.org2.fld.')
-		pytest.raises(TypeError, WFQDN.to_string, 'site1.org2.fld')
-		assert(WFQDN.to_string(WFQDN.from_string(''), leading_dot=True) == '.')
+		assert(WFQDN('site1.org2.fld').to_string(leading_dot=True) == 'site1.org2.fld.')
+		assert(WFQDN.from_string('').to_string(leading_dot=True) == '.')
 
 		assert(isinstance(WFQDN.punycode('\ud13f'), WFQDN) is True)
 		assert(str(WFQDN.punycode('\ud13f')) == 'xn--3v7b')
