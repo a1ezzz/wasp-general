ad4eb16074fc3fbb4dc826de1597edf9c93f5916
Ildar Gafurov <dev@binblob.com>
2019-07-03 22:23:50 +0300

    wasp_general/api/registry.py: new functions and classes for organizing a code

---
+++ tests/wasp_general_api_registry_test.py
@@ -0,0 +1,56 @@
+
+import pytest
+
+from wasp_general.api.registry import WNoSuchAPIIdError, WDuplicateAPIIdError, WAPIRegistryProto, WAPIRegistry
+from wasp_general.api.registry import register_api
+
+
+def test_exceptions():
+	assert(issubclass(WNoSuchAPIIdError, Exception) is True)
+	assert(issubclass(WDuplicateAPIIdError, Exception) is True)
+
+
+def test_abstract():
+	pytest.raises(TypeError, WAPIRegistryProto)
+	pytest.raises(NotImplementedError, WAPIRegistryProto.register, None, None, None)
+	pytest.raises(NotImplementedError, WAPIRegistryProto.get, None, None)
+
+
+class TestWAPIRegistry:
+
+	def test(self):
+		registry = WAPIRegistry()
+		pytest.raises(WNoSuchAPIIdError, registry.get, 'foo')
+		pytest.raises(WNoSuchAPIIdError, registry.get, 'bar')
+
+		registry.register('foo', 1)
+		assert(registry.get('foo') == 1)
+		pytest.raises(WNoSuchAPIIdError, registry.get, 'bar')
+
+		pytest.raises(WDuplicateAPIIdError, registry.register, 'foo', 1)
+
+		registry.register('bar', 1)
+		assert(registry['foo'] == 1)
+		assert(registry['bar'] == 1)
+
+
+def test_register_api():
+
+	def foo(a, b):
+		return a + b
+
+	def bar(a, b):
+		return a - b
+
+	registry = WAPIRegistry()
+
+	decorated_foo = register_api(registry)(foo)
+	decorated_bar = register_api(registry, api_id='zzz')(bar)
+
+	assert(decorated_foo(3, 4) == 7)
+	assert(decorated_bar(3, 4) == -1)
+
+	assert(registry['test_register_api.<locals>.foo'](5, 2) == 7)
+	assert(registry['zzz'](5, 2) == 3)
+
+	pytest.raises(WNoSuchAPIIdError, registry.get, 'bar')
