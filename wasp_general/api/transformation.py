faf11d31cfb7eda5ea836c8bc260b81ce8416e69
Ildar Gafurov <dev@binblob.com>
2019-09-20 10:15:41 +0300

    wasp_general/api/transformation.py: check fix

--- wasp_general/api/transformation.py
+++ wasp_general/api/transformation.py
@@ -101,7 +101,7 @@ class WTransformationRegistry(WAPIRegistry):
 		api_id = self.__api_id(func_type, cls)
 		WAPIRegistry.register(self, api_id, func)
 
-	@verify_type('strict', obj=(object, None))
+	@verify_type('strict', obj_dump=(object, None))
 	def compose(self, obj_dump):
 		""" Create an object from a previously created dump. A dump is a result of
 		:meth:`.WTransformationRegistry.dismantle` method call
