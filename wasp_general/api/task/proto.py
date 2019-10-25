165d8deb24a21e11029a772a1b0a1697d972128c
Ildar Gafurov <dev@binblob.com>
2019-10-25 11:20:40 +0300

    wasp_general/api/task/proto.py: docs update, new excpetions and minor api changes

--- wasp_general/api/task/proto.py
+++ wasp_general/api/task/proto.py
@@ -27,6 +27,24 @@ from wasp_general.api.capability import WCapabilitiesHolder, capability
 from wasp_general.api.registry import WAPIRegistryProto
 
 
+class WNoSuchTask(Exception):
+	""" This exception is raised when a specified by tag and/or a tag with instance id task was not found
+	"""
+	pass
+
+
+class WRequirementsLoop(Exception):
+	""" This exception is raised when there is an attempt to start a tasks with mutual dependencies
+	"""
+	pass
+
+
+class WDependenciesLoop(Exception):
+	""" This exception is raised when there is an attempt to stop a tasks with mutual dependencies
+	"""
+	pass
+
+
 class WTaskProto(WCapabilitiesHolder):
 	""" Basic task prototype. Derived classes must implement the only thing - to start
 	"""
@@ -120,9 +138,9 @@ class WTaskLauncherProto(metaclass=ABCMeta):
 		raise NotImplementedError('This method is abstract')
 
 	@abstractmethod
-	@verify_type('strict', task_tag=str, skip_unresolved=bool, deep_requirements_check=bool)
+	@verify_type('strict', task_tag=str, skip_unresolved=bool, requirements_deep_check=bool)
 	@verify_value('strict', task_tag=lambda x: len(x) > 0)
-	def start_task(self, task_tag, skip_unresolved=False, deep_requirements_check=False):
+	def start_task(self, task_tag, skip_unresolved=False, requirements_deep_check=False):
 		""" Star task from a registry and return it's id (instance id)
 
 		:param task_tag: tag of task that should be started
@@ -131,9 +149,9 @@ class WTaskLauncherProto(metaclass=ABCMeta):
 		:param skip_unresolved: whether a task should be started if all the requirements was not met
 		:type skip_unresolved: bool
 
-		:param deep_requirements_check: whether a recursive requirements check should be made. If this value
+		:param requirements_deep_check: whether a recursive requirements check should be made. If this value
 		is True then requirements of requirements should be checked
-		:type deep_requirements_check: bool
+		:type requirements_deep_check: bool
 
 		:raise WNoSuchTask: raises if the specified task or it's requirements can not be found
 		:raise WRequirementsLoop: raises if there is a mutual dependency between tasks
@@ -146,8 +164,7 @@ class WTaskLauncherProto(metaclass=ABCMeta):
 	@verify_type('strict', task_tag=str, instance_id=(str, None), stop=bool, terminate=bool)
 	@verify_value('strict', task_tag=lambda x: len(x) > 0, instance_id=lambda x: x is None or len(x) > 0)
 	def stop_task(self, task_tag, instance_id=None, stop=True, terminate=False):
-		# TODO: replace "stop" and "terminate" parameters with enum.IntFlag (python>=3.6 is required)
-		""" Stop a previously started task
+		""" Stop a previously started task and return number of tasks instances that were stopped
 
 		:param task_tag: a tag of task that should be stopped
 		:type task_tag: str
@@ -164,7 +181,9 @@ class WTaskLauncherProto(metaclass=ABCMeta):
 		:raise WNoSuchTask: raises if the specified task can not be found
 		:raise WDependenciesLoop: raises if there is a mutual dependency between tasks
 
-		:rtype: None
+		:rtype: int
+
+		TODO: replace "stop" and "terminate" parameters with enum.IntFlag (python>=3.6 is required)
 		"""
 		raise NotImplementedError('This method is abstract')
 
@@ -172,7 +191,8 @@ class WTaskLauncherProto(metaclass=ABCMeta):
 	@verify_type('strict', task_tag=str, stop=bool, terminate=bool)
 	@verify_value('strict', task_tag=lambda x: len(x) > 0)
 	def stop_dependent_tasks(self, task_tag, stop=True, terminate=False):
-		""" Stop tasks that are dependent by a specified one or tasks that are dependent by found dependencies
+		""" Stop tasks that are dependent by a specified one or tasks that are dependent by found dependencies.
+		And return number of tasks instances that were stopped
 
 		:param task_tag: task that will be searched in a requirements of running tasks
 		:type task_tag: str
@@ -183,15 +203,16 @@ class WTaskLauncherProto(metaclass=ABCMeta):
 		:param terminate: whether to call a WTaskProto.terminate capability if is supported by a task
 		:type terminate: bool
 
-		:rtype: None
+		:rtype: int
+
+		TODO: replace "stop" and "terminate" parameters with enum.IntFlag (python>=3.6 is required)
 		"""
 		raise NotImplementedError('This method is abstract')
 
 	@abstractmethod
 	@verify_type('strict', stop=bool, terminate=bool)
 	def all_stop(self, stop=True, terminate=True):
-		# TODO: replace "stop" and "terminate" parameters with enum.IntFlag (python>=3.6 is required)
-		""" Stop all the started tasks
+		""" Stop all the started tasks and return number of tasks instances that were stopped
 
 		:param stop: whether to call a WTaskProto.stop capability if is supported by a task
 		:type stop: bool
@@ -199,6 +220,8 @@ class WTaskLauncherProto(metaclass=ABCMeta):
 		:param terminate: whether to call a WTaskProto.terminate capability if is supported by a task
 		:type terminate: bool
 
-		:rtype: None
+		:rtype: int
+
+		TODO: replace "stop" and "terminate" parameters with enum.IntFlag (python>=3.6 is required)
 		"""
 		raise NotImplementedError('This method is abstract')
