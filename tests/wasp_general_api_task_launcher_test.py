f1e2d9543e5ea417ab60af4ede1d289a4dcf6a99
Ildar Gafurov <dev@binblob.com>
2019-11-06 09:40:06 +0300

    the "aff7f9" commit is merged

---
+++ tests/wasp_general_api_task_launcher_test.py
@@ -0,0 +1,292 @@
+
+import pytest
+
+from wasp_general.api.task.proto import WTaskLauncherProto, WTaskProto, WNoSuchTask, WRequirementsLoop
+from wasp_general.api.task.proto import WDependenciesLoop
+from wasp_general.api.task.registry import WTaskRegistry, __default_task_registry__, register_class
+from wasp_general.api.task.launcher import WTaskLauncher
+
+
+class TestWTaskLauncher:
+
+	start_exec_counter = 0
+	stop_exec_counter = 0
+	terminate_exec_counter = 0
+
+	class BaseTask(WTaskProto):
+		@classmethod
+		def start(cls, **kwargs):
+			TestWTaskLauncher.start_exec_counter += 1
+			return cls()
+
+	class Task1(BaseTask):
+
+		__task_tag__ = 'task1'
+
+	class Task2(BaseTask):
+
+		__task_tag__ = 'task2'
+
+		@classmethod
+		def requirements(cls):
+			return 'task1',
+
+		def stop(self):
+			TestWTaskLauncher.stop_exec_counter += 1
+
+	class Task3(BaseTask):
+		__task_tag__ = 'task3'
+
+		@classmethod
+		def requirements(cls):
+			return 'task1', 'task2'
+
+		def terminate(self):
+			TestWTaskLauncher.terminate_exec_counter += 1
+
+	class Task4(BaseTask):
+		__task_tag__ = 'task4'
+
+		@classmethod
+		def requirements(cls):
+			return 'task3', 'unresolved_task'
+
+	class Task5(BaseTask):
+		__task_tag__ = 'task5'
+
+		@classmethod
+		def requirements(cls):
+			return 'task3', 'task6'  # mutual dependency check
+
+	class Task6(BaseTask):
+		__task_tag__ = 'task6'
+
+		@classmethod
+		def requirements(cls):
+			return 'task1', 'task6'  # mutual dependency check
+
+	def test(self):
+		start_exec_counter = TestWTaskLauncher.start_exec_counter
+
+		launcher = WTaskLauncher()
+		assert(isinstance(launcher, WTaskLauncherProto) is True)
+		assert(launcher.registry() is __default_task_registry__)
+
+		registry = WTaskRegistry()
+		launcher = WTaskLauncher(registry)
+		assert(isinstance(launcher, WTaskLauncherProto) is True)
+		assert(launcher.registry() is registry)
+
+		assert(list(iter(launcher)) == [])
+		assert(list(launcher.started_tasks()) == [])
+		with pytest.raises(WNoSuchTask):
+			list(launcher.started_tasks('task1'))
+
+		register_class(registry=registry)(TestWTaskLauncher.Task1)
+
+		simple_task_instance1 = launcher.start_task('task1')
+		assert(isinstance(simple_task_instance1, str) is True)
+		assert(TestWTaskLauncher.start_exec_counter == (start_exec_counter + 1))
+		simple_task_instance2 = launcher.start_task('task1')
+		assert(isinstance(simple_task_instance2, str) is True)
+		assert(TestWTaskLauncher.start_exec_counter == (start_exec_counter + 2))
+		assert(str(simple_task_instance1) != str(simple_task_instance2))
+
+		tasks_list = list(iter(launcher))
+		tasks_str_instances = [x[1] for x in tasks_list]
+		assert([x[0] for x in tasks_list] == ['task1', 'task1'])
+		assert(simple_task_instance1 in tasks_str_instances)
+		assert(simple_task_instance2 in tasks_str_instances)
+
+		pytest.raises(WNoSuchTask, launcher.start_task, 'unknown_task')
+		pytest.raises(WNoSuchTask, launcher.stop_task, 'unknown_task')
+
+		assert(launcher.stop_task('task1') == 2)
+
+	def test_requirements(self):
+		start_exec_counter = TestWTaskLauncher.start_exec_counter
+
+		registry = WTaskRegistry()
+		register_class(registry=registry)(TestWTaskLauncher.Task1)
+		register_class(registry=registry)(TestWTaskLauncher.Task2)
+		register_class(registry=registry)(TestWTaskLauncher.Task3)
+		register_class(registry=registry)(TestWTaskLauncher.Task4)
+		register_class(registry=registry)(TestWTaskLauncher.Task5)
+		register_class(registry=registry)(TestWTaskLauncher.Task6)
+
+		launcher = WTaskLauncher(registry)
+		assert(launcher.requirements('task1') is None)
+		assert(set(launcher.requirements('task2')) == {'task1'})
+		assert(set(launcher.requirements('task3')) == {'task1', 'task2'})
+
+		task3_instance = launcher.start_task('task3')
+		tasks_tags = tuple(x[0] for x in launcher)
+		assert(len(tasks_tags) == 3)
+		assert('task1' in tasks_tags)
+		assert('task2' in tasks_tags)
+		assert('task3' in tasks_tags)
+		assert(TestWTaskLauncher.start_exec_counter == (start_exec_counter + 3))
+
+		assert(len(tuple(launcher.started_tasks('task1'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task2'))) == 1)
+		assert(tuple(launcher.started_tasks('task3')) == (('task3', task3_instance),))
+
+		launcher.start_task('task2')
+		tasks_tags = tuple(x[0] for x in launcher)
+		assert(len(tasks_tags) == 4)
+		assert(len(tuple(launcher.started_tasks('task1'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task2'))) == 2)
+		assert(len(tuple(launcher.started_tasks('task3'))) == 1)
+		assert(TestWTaskLauncher.start_exec_counter == (start_exec_counter + 4))
+
+		pytest.raises(WNoSuchTask, launcher.start_task, 'task4')
+		launcher.start_task('task4', skip_unresolved=True)
+		tasks_tags = tuple(x[0] for x in launcher)
+		assert(len(tasks_tags) == 5)
+		assert(len(tuple(launcher.started_tasks('task1'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task2'))) == 2)
+		assert(len(tuple(launcher.started_tasks('task3'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task4'))) == 1)
+		assert(TestWTaskLauncher.start_exec_counter == (start_exec_counter + 5))
+
+		assert(launcher.stop_task('task1') == 1)
+		assert(launcher.stop_task('task4') == 1)
+		tasks_tags = tuple(x[0] for x in launcher)
+		assert(len(tasks_tags) == 3)
+		assert(len(tuple(launcher.started_tasks('task2'))) == 2)
+		assert(len(tuple(launcher.started_tasks('task3'))) == 1)
+
+		launcher.start_task('task4', skip_unresolved=True)
+		tasks_tags = tuple(x[0] for x in launcher)
+		assert(len(tasks_tags) == 4)
+		assert(len(tuple(launcher.started_tasks('task2'))) == 2)
+		assert(len(tuple(launcher.started_tasks('task3'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task4'))) == 1)
+		assert(TestWTaskLauncher.start_exec_counter == (start_exec_counter + 6))
+
+		launcher.start_task('task4', skip_unresolved=True, requirements_deep_check=True)
+		tasks_tags = tuple(x[0] for x in launcher)
+		assert(len(tasks_tags) == 6)
+		assert(len(tuple(launcher.started_tasks('task1'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task2'))) == 2)
+		assert(len(tuple(launcher.started_tasks('task3'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task4'))) == 2)
+		assert(TestWTaskLauncher.start_exec_counter == (start_exec_counter + 8))
+
+		pytest.raises(WRequirementsLoop, launcher.start_task, 'task5')
+		pytest.raises(WRequirementsLoop, launcher.start_task, 'task6')
+
+	def test_stop(self):
+		stop_exec_counter = TestWTaskLauncher.stop_exec_counter
+		terminate_exec_counter = TestWTaskLauncher.terminate_exec_counter
+
+		registry = WTaskRegistry()
+		register_class(registry=registry)(TestWTaskLauncher.Task1)
+		register_class(registry=registry)(TestWTaskLauncher.Task2)
+		register_class(registry=registry)(TestWTaskLauncher.Task3)
+		register_class(registry=registry)(TestWTaskLauncher.Task4)
+
+		launcher = WTaskLauncher(registry)
+		launcher.start_task('task3')
+		launcher.start_task('task4', skip_unresolved=True)
+		launcher.start_task('task2')
+
+		tasks_tags = tuple(x[0] for x in launcher)
+		assert(len(tasks_tags) == 5)
+		assert(len(tuple(launcher.started_tasks('task1'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task2'))) == 2)
+		assert(len(tuple(launcher.started_tasks('task3'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task4'))) == 1)
+
+		assert(launcher.stop_task('task1') == 1)
+		assert(TestWTaskLauncher.stop_exec_counter == stop_exec_counter)
+		assert(TestWTaskLauncher.terminate_exec_counter == terminate_exec_counter)
+
+		assert(launcher.stop_task('task2') == 2)
+		assert(TestWTaskLauncher.stop_exec_counter == (stop_exec_counter + 2))
+		# note: there are two instances of 'task2'
+		assert(TestWTaskLauncher.terminate_exec_counter == terminate_exec_counter)
+
+		assert(launcher.stop_task('task3') == 1)
+		assert(TestWTaskLauncher.stop_exec_counter == (stop_exec_counter + 2))
+		assert(TestWTaskLauncher.terminate_exec_counter == terminate_exec_counter)
+
+		launcher.start_task('task3')
+		assert(launcher.stop_task('task3', terminate=True) == 1)
+		assert(TestWTaskLauncher.stop_exec_counter == (stop_exec_counter + 2))
+		assert(TestWTaskLauncher.terminate_exec_counter == (terminate_exec_counter + 1))
+
+		launcher.start_task('task3')
+		task2_instance = launcher.start_task('task2')
+		tasks_tags = tuple(x[0] for x in launcher)
+		assert(len(tasks_tags) == 5)
+		assert(len(tuple(launcher.started_tasks('task1'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task2'))) == 2)
+		assert(len(tuple(launcher.started_tasks('task3'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task4'))) == 1)
+
+		assert(launcher.stop_task('task2', instance_id=task2_instance) == 1)
+		assert(TestWTaskLauncher.stop_exec_counter == (stop_exec_counter + 3))
+		assert(TestWTaskLauncher.terminate_exec_counter == (terminate_exec_counter + 1))
+		tasks_tags = tuple(x[0] for x in launcher)
+		assert(len(tasks_tags) == 4)
+		assert(len(tuple(launcher.started_tasks('task1'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task2'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task3'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task4'))) == 1)
+
+		pytest.raises(WNoSuchTask, launcher.stop_task, 'task2', 'unknown_instance')
+
+		assert(launcher.stop_task('task2', stop=False) == 1)
+		assert(TestWTaskLauncher.stop_exec_counter == (stop_exec_counter + 3))
+		assert(TestWTaskLauncher.terminate_exec_counter == (terminate_exec_counter + 1))
+		tasks_tags = tuple(x[0] for x in launcher)
+		assert(len(tasks_tags) == 3)
+		assert(len(tuple(launcher.started_tasks('task1'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task3'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task4'))) == 1)
+
+		assert(launcher.stop_dependent_tasks('task1') == 2)
+		assert(TestWTaskLauncher.stop_exec_counter == (stop_exec_counter + 3))
+		assert(TestWTaskLauncher.terminate_exec_counter == (terminate_exec_counter + 1))
+		tasks_tags = tuple(x[0] for x in launcher)
+		assert(len(tasks_tags) == 1)
+		assert(len(tuple(launcher.started_tasks('task1'))) == 1)
+
+		class Task7(TestWTaskLauncher.BaseTask):
+			__task_tag__ = 'task7'
+			__requirements__ = None
+
+			@classmethod
+			def requirements(cls):
+				return cls.__requirements__
+
+		class Task8(TestWTaskLauncher.BaseTask):
+			__task_tag__ = 'task8'
+
+			@classmethod
+			def requirements(cls):
+				return 'task7', 'task1'
+
+		register_class(registry=registry)(Task7)
+		register_class(registry=registry)(Task8)
+
+		launcher.start_task('task8')
+		tasks_tags = tuple(x[0] for x in launcher)
+		assert(len(tasks_tags) == 3)
+		assert(len(tuple(launcher.started_tasks('task1'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task7'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task8'))) == 1)
+
+		Task7.__requirements__ = ('task8', )
+		pytest.raises(WDependenciesLoop, launcher.stop_dependent_tasks, 'task1')
+
+		Task7.__requirements__ = None
+		tasks_tags = tuple(x[0] for x in launcher)
+		assert(len(tasks_tags) == 3)
+		assert(len(tuple(launcher.started_tasks('task1'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task7'))) == 1)
+		assert(len(tuple(launcher.started_tasks('task8'))) == 1)
+
+		launcher.all_stop()
+		assert(list(launcher) == [])
--- tests/wasp_general_api_task_proto_test.py
+++ tests/wasp_general_api_task_proto_test.py
@@ -14,50 +14,36 @@ def test_exceptions():
 
 
 def test_abstract_classes():
-
-	class Registry(WTaskRegistryProto):
-
-		def get(self, api_id):
-			pass
-
-		def has(self, api_id):
-			pass
-
-		def register(self, api_id, api_descriptor):
-			pass
-
-		def unregister(self, api_id):
-			pass
-
-		def ids(self):
-			pass
-
 	pytest.raises(TypeError, WTaskProto)
-	pytest.raises(NotImplementedError, WTaskProto.start, None)
+	pytest.raises(NotImplementedError, WTaskProto.start)
 
 	pytest.raises(TypeError, WTaskRegistryProto)
 	pytest.raises(NotImplementedError, WTaskRegistryProto.register, None, 'task_id', WTaskProto)
 
 	pytest.raises(TypeError, WTaskLauncherProto)
 	pytest.raises(NotImplementedError, WTaskLauncherProto.started_tasks, None)
+	pytest.raises(NotImplementedError, WTaskLauncherProto.registry, None)
 	pytest.raises(
-		NotImplementedError, WTaskLauncherProto.start_task, None, Registry(), 'foo'
+		NotImplementedError, WTaskLauncherProto.start_task, None, 'foo'
 	)
 	pytest.raises(NotImplementedError, WTaskLauncherProto.stop_task, None, 'foo')
+	pytest.raises(NotImplementedError, WTaskLauncherProto.stop_dependent_tasks, None, 'foo')
 	pytest.raises(NotImplementedError, WTaskLauncherProto.all_stop, None)
 
 
 class TestWTaskProto:
+
+	class Task(WTaskProto):
+
+		@classmethod
+		def start(cls, **kwargs):
+			return TestWTaskProto.Task()
 
 	def test(self):
 		assert(WTaskProto.requirements() is None)
 		assert(isinstance(WTaskProto.stop, WCapabilityDescriptor) is True)
 		assert(isinstance(WTaskProto.terminate, WCapabilityDescriptor) is True)
 
-		class Task(WTaskProto):
-			def start(cls, *args, **kwargs):
-				pass
-
-		task = Task()
+		task = TestWTaskProto.Task()
 		pytest.raises(NotImplementedError, task.stop)
 		pytest.raises(NotImplementedError, task.terminate)
