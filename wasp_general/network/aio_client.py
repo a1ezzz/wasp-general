c695c0d152ff8b0d764f979a4a0d526af35909e1
Ildar Gafurov <dev@binblob.com>
2021-08-16 22:46:20 +0300

    wasp_general.network.aio_client: tcp client support

--- wasp_general/network/aio_client.py
+++ wasp_general/network/aio_client.py
@@ -103,27 +103,45 @@ class WDatagramProtocol(asyncio.DatagramProtocol, metaclass=ABCMeta):
         raise NotImplementedError('This method is abstract')
 
 
-@register_api(__default_network_client_collection__, 'udp')
-class WDatagramNetworkClient(AIONetworkClientProto):
-    """ Network client that runs over UDP in (obviously) datagram mode
+class WStreamProtocol(asyncio.Protocol, metaclass=ABCMeta):
+    """ Prototype for a protocol that is used along with TCP
+    """
+
+    @abstractmethod
+    async def session_complete(self):
+        """ This coroutine is completed when job is done and connection should be terminated
+
+        :return: Connection result (will be returned via AIONetworkClientProto.connect)
+        :rtype: any
+        """
+        raise NotImplementedError('This method is abstract')
+
+
+# noinspection PyAbstractClass
+class WBaseNetworkClient(AIONetworkClientProto):
+    """ This class helps to implement a real network client
+    """
+
+    __supported_protocol__ = asyncio.BaseProtocol
+    """ This is a protocol class, that derived classes (services) are awaiting for
     """
 
     @verify_type('strict', uri=WURI, bind_uri=(WURI, None), aio_loop=(asyncio.AbstractEventLoop, None))
     @verify_type('strict', socket_collection=(WAPIRegistryProto, None))
-    @verify_subclass(protocol_cls=WDatagramProtocol)
+    @verify_subclass(protocol_cls=__supported_protocol__)
     def __init__(self, uri, protocol_cls, bind_uri=None, aio_loop=None, socket_collection=None):
         """ Create a new network client
 
-        :param uri: URI with address where this client is connected to.
+        :param uri: URI with which socket is opened and with which a related client or service is instantiated
         :type uri: WURI
 
         :param protocol_cls: protocol that do a real work
-        :type protocol_cls: asyncio.DatagramProtocol
+        :type protocol_cls: asyncio.BaseProtocol
 
         :param bind_uri: URI with which socket should be bound to
         :type bind_uri: WURI | str | None
 
-        :param aio_loop: a loop with which network client will work (by default a current loop is used)
+        :param aio_loop: a loop with which network service will work (by default a current loop is used)
         :type aio_loop: asyncio.AbstractEventLoop | None
 
         :param socket_collection: collection with which socket is opened (by default the
@@ -131,26 +149,61 @@ class WDatagramNetworkClient(AIONetworkClientProto):
         :type socket_collection: WAPIRegistryProto | None
         """
         AIONetworkClientProto.__init__(self)
-        self.__uri = uri
-        self.__protocol_cls = protocol_cls
-        self.__bind_uri = bind_uri
-        self.__aio_loop = aio_loop
-        self.__socket_collection = socket_collection if socket_collection else __default_socket_collection__
+        self._uri = uri
+        self._protocol_cls = protocol_cls
+        self._bind_uri = bind_uri
+        self._socket_collection = socket_collection if socket_collection else __default_socket_collection__
+        self._aio_loop = aio_loop if aio_loop else asyncio.get_event_loop()
+        self._transport = None
+
+
+@register_api(__default_network_client_collection__, 'udp')
+class WUDPNetworkClient(WBaseNetworkClient):
+    """ Network client that runs over UDP in (obviously) datagram mode
+    """
+
+    __supported_protocol__ = WDatagramProtocol
+    """ This service require datagram protocol
+    """
 
     async def connect(self):
         """ :meth:`.WDatagramProtocol.connect` implementation
         :rtype: any
         """
-        loop = self.__aio_loop if self.__aio_loop else asyncio.get_event_loop()
-        socket_handler = self.__socket_collection.open(self.__uri)
-        sock = socket_handler.socket()
-        if self.__bind_uri:
-            sock.bind((self.__bind_uri.hostname(), self.__bind_uri.port()))
-        sock.connect((self.__uri.hostname(), self.__uri.port()))
-        sock.setblocking(False)
+        sock = self._socket_collection.aio_socket(self._uri)
+        if self._bind_uri:
+            sock.bind((self._bind_uri.hostname(), self._bind_uri.port()))
+        await self._aio_loop.sock_connect(sock, (self._uri.hostname(), self._uri.port()))
 
-        transport, protocol = await loop.create_datagram_endpoint(self.__protocol_cls, sock=sock)
+        transport, protocol = await self._aio_loop.create_datagram_endpoint(self._protocol_cls, sock=sock)
 
         result = await protocol.session_complete()
         transport.close()
+        sock.close()
+        return result
+
+
+@register_api(__default_network_client_collection__, 'tcp')
+class WTCPNetworkClient(WBaseNetworkClient):
+    """ Network client that runs over TCP in (obviously) stream mode
+    """
+
+    __supported_protocol__ = WStreamProtocol
+    """ This service require stream protocol
+    """
+
+    async def connect(self):
+        """ :meth:`.WDatagramProtocol.connect` implementation
+        :rtype: any
+        """
+        sock = self._socket_collection.aio_socket(self._uri)
+        if self._bind_uri:
+            sock.bind((self._bind_uri.hostname(), self._bind_uri.port()))
+        await self._aio_loop.sock_connect(sock, (self._uri.hostname(), self._uri.port()))
+
+        transport, protocol = await self._aio_loop.create_connection(self._protocol_cls, sock=sock)
+
+        result = await protocol.session_complete()
+        transport.close()
+        sock.close()
         return result
