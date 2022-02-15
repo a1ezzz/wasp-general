498fa9ab28fd21152da78d5a6edbf05b4263e31c
Ildar Gafurov <dev@binblob.com>
2022-02-15 20:23:36 +0300

    wasp_general.api.task.proto.py: more exceptions, updated signals, updated WSchedulerProto

--- wasp_general/api/task/proto.py
+++ wasp_general/api/task/proto.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# wasp_general/task/proto.py
+# wasp_general/api/task/proto.py
 #
 # Copyright (C) 2016-2019, 2022 the wasp-general authors and contributors
 # <see AUTHORS file>
@@ -22,7 +22,7 @@
 from abc import ABCMeta, abstractmethod
 import enum
 from dataclasses import dataclass
-from typing import Any
+import typing
 
 from wasp_general.verify import verify_type, verify_value
 
@@ -43,6 +43,25 @@ class WDependenciesLoopError(Exception):
 	pass
 
 
+class WTaskStartError(Exception):
+	""" This exception is raised when there is an error in starting a task
+	"""
+	pass
+
+
+class WTaskStopError(Exception):
+	""" This exception is raised when there is an error in stopping a task
+	"""
+	pass
+
+
+class WNoSuchTaskError(Exception):
+	""" This exception is raised when there is no requested task (it may a request to start unknown task or to stop
+	already stopped task)
+	"""
+	pass
+
+
 class WCapabilitiesSignalsMeta(WSignalSourceMeta, WCapabilitiesHolderMeta):
 	""" This metaclass is for classes that may send signals and may have capabilities
 	"""
@@ -58,14 +77,25 @@ class WCapabilitiesSignalsMeta(WSignalSourceMeta, WCapabilitiesHolderMeta):
 		WCapabilitiesHolderMeta.__init__(cls, name, bases, namespace)
 
 
+@dataclass
+class WTaskResult:
+	""" This class is used with a completion signal defining a result of a completed task. In order to check whether a
+	task was completed successfully the 'exception' property should be checked
+
+	:note: the 'result' property must be the same as the :meth:`WTaskProto.start` method call
+	"""
+	result: typing.Any = None        # a result of completed record (if any)
+	exception: BaseException = None  # an exception raised within a task (if any)
+
+
 class WTaskProto(WSignalSource, WCapabilitiesHolder, metaclass=WCapabilitiesSignalsMeta):
-	""" Basic task prototype. Derived classes must implement the only thing - to start
+	""" Basic task prototype. Derived classes must implement the only thing - :meth:`WTaskProto.start`
 	"""
 
-	task_started = WSignal(None)     # a task started
-	task_completed = WSignal()       # a task completed. A signal may have a result of a task
-	task_stopped = WSignal(None)     # a task stopped
-	task_terminated = WSignal(None)  # a task terminated
+	task_started = WSignal(None)           # a task started
+	task_completed = WSignal(WTaskResult)  # a task completed
+	task_stopped = WSignal(None)           # a task stopped
+	task_terminated = WSignal(None)        # a task terminated
 
 	@abstractmethod
 	def start(self):
@@ -98,7 +128,7 @@ class WTaskProto(WSignalSource, WCapabilitiesHolder, metaclass=WCapabilitiesSign
 
 @enum.unique
 class WTaskStopMode(enum.Flag):
-	""" Defines ways to stop a task
+	""" Defines a way to stop a task
 	"""
 	stop = enum.auto()       # stop a task gracefully
 	terminate = enum.auto()  # terminate a task
@@ -107,14 +137,17 @@ class WTaskStopMode(enum.Flag):
 # noinspection PyAbstractClass
 class WLauncherTaskProto(WTaskProto):
 	""" This is a task prototype that may be launched by an :class:`.WLauncherProto` instance. Each type of tasks
-	identified by unique string (called tag)
+	identified by unique string (called tag) this tag must be redefined in derived classes
 	"""
+
+	__task_tag__ = None  # this is a unique identifier of a task. Must be redefined in derived classes
 
 	@classmethod
 	@abstractmethod
 	def launcher_task(cls):
 		""" Create and return a task that later will be started by a specified launcher
-		:rtype: WLauncherTaskProto
+
+		:rtype: WTaskProto
 		"""
 
 		raise NotImplementedError('This method is abstract')
@@ -155,9 +188,9 @@ class WLauncherProto(WAPIRegistry):
 		raise NotImplementedError('This method is abstract')
 
 	@abstractmethod
-	@verify_type('strict', task_tag=str, skip_unresolved=bool, requirements_deep_check=bool)
+	@verify_type('strict', task_tag=str, skip_unresolved=bool)
 	@verify_value('strict', task_tag=lambda x: len(x) > 0)
-	def start_task(self, task_tag, skip_unresolved=False, requirements_deep_check=False):
+	def start_task(self, task_tag, skip_unresolved=False):
 		""" Star a task and its requirements. Return number of tasks that were started
 
 		:param task_tag: tag of task that should be started
@@ -166,13 +199,9 @@ class WLauncherProto(WAPIRegistry):
 		:param skip_unresolved: whether a task should be started if all the requirements was not met
 		:type skip_unresolved: bool
 
-		:param requirements_deep_check: whether a recursive requirements check should be made. If this value
-		is True then requirements of requirements should be checked
-		:type requirements_deep_check: bool
-
-		:raise WNoSuchTask: raises if the specified task or it's requirements can not be found
-		:raise WStartedTaskError: if a task is started already
-		:raise WRequirementsLoop: raises if there is a mutual dependency between tasks
+		:raise WNoSuchTaskError: raises if the specified task or it's requirements can not be found
+		:raise WTaskStartError: if a task is started already
+		:raise WRequirementsLoopError: raises if some tasks require each other
 
 		:rtype: int
 		"""
@@ -190,8 +219,7 @@ class WLauncherProto(WAPIRegistry):
 		:param stop_mode: whether a task should be stopped gracefully
 		:type stop_mode: WTaskStopMode
 
-		:raise WNoSuchTask: raises if the specified task can not be found
-		:raise WDependenciesLoop: raises if there is a mutual dependency between tasks
+		:raise WNoSuchTask: raises if task isn't running
 
 		:rtype: None
 		"""
@@ -210,6 +238,8 @@ class WLauncherProto(WAPIRegistry):
 		:param stop_mode: whether tasks should be stopped gracefully
 		:type stop_mode: WTaskStopMode
 
+		:raise WDependenciesLoopError: raises if there is a mutual dependency between tasks
+
 		:rtype: int
 		"""
 		raise NotImplementedError('This method is abstract')
@@ -221,6 +251,8 @@ class WLauncherProto(WAPIRegistry):
 
 		:param stop_mode: whether tasks should be stopped gracefully
 		:type stop_mode: WTaskStopMode
+
+		:raise WDependenciesLoop: raises if there is a mutual dependency between tasks
 
 		:rtype: int
 		"""
@@ -243,10 +275,9 @@ class WScheduleRecordProto(metaclass=ABCMeta):
 	(should start). It has a :class:`.WScheduledTaskProto` task to be started and postpone policy
 	(:class:`.WTaskPostponePolicy`)
 
-	Postpone policy will be applied to this task and to tasks with the same group id (if it was set). Postpone
-	policy is a recommendation for a scheduler and a scheduler can omit it if (for example) a scheduler queue
-	is full. 'WScheduledTaskPostponePolicy.keep_first' and 'WScheduledTaskPostponePolicy.keep_last' policies are
-	ignored without group id
+	Postpone policy is a recommendation for a scheduler and a scheduler can omit it if (for example) a scheduler queue
+	is full. 'WScheduledTaskPostponePolicy.keep_first' and 'WScheduledTaskPostponePolicy.keep_last' postpone policies
+	(so as simultaneous policy) will be applied to this task and to tasks with the same group id (if it was set).
 	"""
 
 	@abstractmethod
@@ -305,19 +336,11 @@ class WScheduleSourceProto(WSignalSource):
 
 
 @dataclass
-class WTaskCrashReason:
-	""" Used by scheduler signal
+class WScheduledTaskResult:
+	""" This class is used by scheduler signal defining a result of a completed record
 	"""
-	record: WScheduleRecordProto  # record that cause exception
-	exception: BaseException      # raised exception
-
-
-@dataclass
-class WTaskResult:
-	""" Used by scheduler signal
-	"""
-	record: WScheduleRecordProto  # record that return value
-	result: Any = None            # returned value
+	record: WScheduleRecordProto  # completed record
+	task_result: WTaskResult      # a result of completed record
 
 
 # noinspection PyAbstractClass
@@ -331,10 +354,8 @@ class WSchedulerProto(WTaskProto):
 	scheduled_task_postponed = WSignal(WScheduleRecordProto)  # a scheduled task dropped and will start later
 	scheduled_task_expired = WSignal(WScheduleRecordProto)    # a scheduled task dropped because of expired ttl
 	scheduled_task_started = WSignal(WScheduleRecordProto)    # a scheduled task started
-	scheduled_task_crashed = WSignal(WTaskCrashReason)        # a scheduled task crashed
-	scheduled_task_completed = WSignal(WTaskResult)           # a scheduled task completed
-	scheduled_task_stopped = WSignal(None)                    # a stop request for a scheduled task completed
-	scheduled_task_terminated = WSignal(None)                 # a termination request for a scheduled task completed
+	scheduled_task_completed = WSignal(WScheduledTaskResult)  # a scheduled task completed
+	scheduled_task_stopped = WSignal(WScheduleRecordProto)    # a stop request for a scheduled task completed
 
 	@abstractmethod
 	@verify_type('strict', schedule_source=WScheduleSourceProto)
@@ -357,13 +378,5 @@ class WSchedulerProto(WTaskProto):
 		:type schedule_source: WTaskSourceProto
 
 		:rtype: None
-		"""
-		raise NotImplementedError('This method is abstract')
-
-	@abstractmethod
-	def running_records(self):
-		""" Return generator that will iterate over running tasks (records)
-
-		:rtype: generator (of WScheduleRecordProto instances)
 		"""
 		raise NotImplementedError('This method is abstract')
