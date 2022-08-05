4f465842030967bc5d28bd90da917b2759e68a15
Ildar Gafurov <dev@binblob.com>
2022-08-05 15:38:08 +0300

    Feature/socket factory part4 (#17)
    * some commit is merged =)
    
    * the "bfab607b" commit is merged
    
    * the "bfc0ed05" commit is merged

---
+++ tests/wasp_general_api_task_launcher_test.py
@@ -0,0 +1,256 @@
+
+import pytest
+
+from wasp_general.api.registry import WAPIRegistry
+from wasp_general.api.task.proto import WLauncherTaskProto, WLauncherProto, WNoSuchTaskError, WTaskStartError
+from wasp_general.api.task.proto import WRequirementsLoopError, WDependenciesLoopError, WTaskStopMode
+from wasp_general.api.task.launcher import WLauncher, register_task
+
+
+class TestData:
+
+	launch_counter = 0
+	stop_exec_counter = 0
+	terminate_exec_counter = 0
+
+	registry = WAPIRegistry()
+
+	class BaseTask(WLauncherTaskProto):
+		@classmethod
+		def launcher_task(cls):
+			TestData.launch_counter += 1
+			return cls()
+
+		def start(self):
+			pass
+
+	@register_task(registry)
+	class Task1(BaseTask):
+		__task_tag__ = 'task1'
+
+	@register_task(registry)
+	class Task2(BaseTask):
+		__task_tag__ = 'task2'
+
+		@classmethod
+		def requirements(cls):
+			return 'task1',
+
+		def stop(self):
+			TestData.stop_exec_counter += 1
+
+	@register_task(registry)
+	class Task3(BaseTask):
+		__task_tag__ = 'task3'
+
+		@classmethod
+		def requirements(cls):
+			return 'task1', 'task2'
+
+		def terminate(self):
+			TestData.terminate_exec_counter += 1
+
+	@register_task(registry)
+	class Task4(BaseTask):
+		__task_tag__ = 'task4'
+
+		@classmethod
+		def requirements(cls):
+			return 'task3', 'unresolved_task'
+
+	@register_task(registry)
+	class Task5(BaseTask):
+		__task_tag__ = 'task5'
+
+		@classmethod
+		def requirements(cls):
+			return 'task3', 'task6'  # mutual dependency check
+
+	@register_task(registry)
+	class Task6(BaseTask):
+		__task_tag__ = 'task6'
+
+		@classmethod
+		def requirements(cls):
+			return 'task1', 'task6'  # mutual dependency check
+
+	@register_task(registry)
+	class Task7(BaseTask):
+		__task_tag__ = 'task7'
+		__requirements__ = None
+
+		@classmethod
+		def requirements(cls):
+			return cls.__requirements__
+
+	@register_task(registry)
+	class Task8(BaseTask):
+		__task_tag__ = 'task8'
+
+		@classmethod
+		def requirements(cls):
+			return 'task7', 'task1'
+
+
+class TestWTaskLauncher:
+
+	def test(self):
+		start_exec_counter = TestData.launch_counter
+		launcher = WLauncher(TestData.registry)
+		assert(isinstance(launcher, WLauncherProto) is True)
+
+		assert(len(launcher) == 0)
+		assert(list(launcher) == [])
+		assert(len(list(launcher.started_tasks())) == 0)
+		assert(list(launcher.started_tasks()) == [])
+
+		assert(launcher.is_started('task1') is False)
+		assert('task1' not in launcher)
+
+		assert(launcher.start_task('task1') == 1)
+		assert(TestData.launch_counter == (start_exec_counter + 1))
+		assert(launcher.is_started('task1') is True)
+		assert('task1' in launcher)
+
+		pytest.raises(WTaskStartError, launcher.start_task, 'task1')
+		assert(len(launcher) == 1)
+		assert(list(launcher) == ['task1'])
+		assert(launcher.is_started('task1') is True)
+		assert('task1' in launcher)
+
+		pytest.raises(WNoSuchTaskError, launcher.start_task, 'unknown_task')
+		pytest.raises(WNoSuchTaskError, launcher.stop_task, 'unknown_task')
+
+		launcher.stop_task('task1')
+		assert(len(launcher) == 0)
+
+	def test_requirements(self):
+		launch_counter = TestData.launch_counter
+		launcher = WLauncher(TestData.registry)
+
+		assert(launcher.start_task('task3') == 3)
+		assert(len(launcher) == 3)
+		assert('task2' in launcher)
+		assert('task2' in launcher)
+		assert('task3' in launcher)
+		assert(TestData.launch_counter == (launch_counter + 3))
+
+		pytest.raises(WTaskStartError, launcher.start_task, 'task2')
+		assert(len(tuple(launcher)) == 3)
+
+		pytest.raises(WNoSuchTaskError, launcher.start_task, 'task4')
+		assert(launcher.start_task('task4', skip_unresolved=True) == 1)
+		assert(len(launcher) == 4)
+		assert('task1' in launcher)
+		assert('task2' in launcher)
+		assert('task3' in launcher)
+		assert('task4' in launcher)
+		assert(TestData.launch_counter == (launch_counter + 4))
+
+		launcher.stop_task('task1')
+		launcher.stop_task('task4')
+		assert(len(launcher) == 2)
+		assert('task2' in launcher)
+		assert('task3' in launcher)
+
+		assert(launcher.start_task('task4', skip_unresolved=True) == 1)
+		assert(len(launcher) == 3)
+		assert('task2' in launcher)
+		assert('task3' in launcher)
+		assert('task4' in launcher)
+		assert(TestData.launch_counter == (launch_counter + 5))
+
+		launcher.stop_task('task4')
+		assert(len(launcher) == 2)
+
+		pytest.raises(WRequirementsLoopError, launcher.start_task, 'task5')
+		pytest.raises(WRequirementsLoopError, launcher.start_task, 'task6')
+
+	def test_stop(self):
+		stop_exec_counter = TestData.stop_exec_counter
+		terminate_exec_counter = TestData.terminate_exec_counter
+
+		launcher = WLauncher(TestData.registry)
+
+		assert(launcher.start_task('task3') == 3)
+		assert(launcher.start_task('task4', skip_unresolved=True) == 1)
+
+		launcher.stop_task('task1')
+		assert(TestData.stop_exec_counter == stop_exec_counter)
+		assert(TestData.terminate_exec_counter == terminate_exec_counter)
+
+		launcher.stop_task('task2')
+		assert(TestData.stop_exec_counter == (stop_exec_counter + 1))
+		assert(TestData.terminate_exec_counter == terminate_exec_counter)
+
+		launcher.stop_task('task3')
+		assert(TestData.stop_exec_counter == (stop_exec_counter + 1))
+		assert(TestData.terminate_exec_counter == terminate_exec_counter)
+
+		launcher.start_task('task3')
+		launcher.stop_task('task3', stop_mode=WTaskStopMode.terminate)
+		assert(TestData.stop_exec_counter == (stop_exec_counter + 1))
+		assert(TestData.terminate_exec_counter == (terminate_exec_counter + 1))
+
+		pytest.raises(WNoSuchTaskError, launcher.stop_task, 'task3')
+
+		launcher.stop_task('task2')
+		assert(TestData.stop_exec_counter == (stop_exec_counter + 2))
+		assert(TestData.terminate_exec_counter == (terminate_exec_counter + 1))
+
+		launcher.start_task('task3')
+		assert(len(launcher) == 4)
+		assert('task1' in launcher)
+		assert('task2' in launcher)
+		assert('task3' in launcher)
+		assert('task4' in launcher)
+
+		assert(launcher.stop_dependent_tasks('task1') == 3)
+		assert(TestData.stop_exec_counter == (stop_exec_counter + 3))
+		assert(TestData.terminate_exec_counter == (terminate_exec_counter + 1))
+		assert(list(launcher) == ['task1'])
+
+		launcher.start_task('task8')
+		assert(len(launcher) == 3)
+		assert('task1' in launcher)
+		assert('task7' in launcher)
+		assert('task8' in launcher)
+
+		TestData.Task7.__requirements__ = ('task8', )
+		pytest.raises(WDependenciesLoopError, launcher.stop_dependent_tasks, 'task1')
+
+		TestData.Task7.__requirements__ = None
+		assert(launcher.all_stop() == 3)
+		assert(list(launcher) == [])
+
+
+def test_register_task():
+	registry = WAPIRegistry()
+	assert(registry.has(TestData.Task1.__task_tag__) is False)
+
+	register_task(registry=registry)(TestData.Task1)
+
+	assert(registry.has(TestData.Task1.__task_tag__) is True)
+	assert(registry.get(TestData.Task1.__task_tag__) is TestData.Task1)
+
+	class CorrupterTask(WLauncherTaskProto):
+		# there is no redefined __task_tag__
+
+		@classmethod
+		def launcher_task(cls,):
+			TestData.launch_counter += 1
+			return cls()
+
+		def start(self):
+			pass
+
+	with pytest.raises(ValueError):
+		register_task(registry=registry)(CorrupterTask)
+
+	CorrupterTask.__task_tag__ = 1  # not it has a tag, but it has invalid type
+
+	with pytest.raises(ValueError):
+		register_task(registry=registry)(CorrupterTask)
+
+	CorrupterTask.__task_tag__ = 'task'  # now it is ok
+	register_task(registry=registry)(CorrupterTask)
--- wasp_general/thread.py
+++ wasp_general/thread.py
@@ -19,6 +19,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with wasp-general.  If not, see <http://www.gnu.org/licenses/>.
 
+from functools import partial
 from threading import Lock
 from decorator import decorator
 
@@ -31,28 +32,59 @@ class WCriticalSectionError(Exception):
 	pass
 
 
-@verify_type(blocking=bool, timeout=(int, float, None), raise_exception=bool)
-@verify_value(lock_fn=lambda x: callable(x))
-@verify_value(timeout=lambda x: x is None or x > 0)
-def critical_section_dynamic_lock(lock_fn, blocking=True, timeout=None, raise_exception=True):
+@verify_type('strict', timeout=(int, float, None), lock=Lock().__class__)
+@verify_value('strict', lock_fn=lambda x: callable(x))
+def acquire_lock(lock, timeout=None):
+	""" This function overload the original :meth:`.Lock.acquire` method that has two arguments ('blocking'
+	and 'timeout') with the one argument only - 'timeout'. This argument may be set to None now
+
+	:param lock: a lock that should be gained
+	:type lock: Lock
+
+	:param timeout: timeout that this function should wait in order to gain a lock. If the "timeout" is not
+	specified (is None) then wait forever in a blocking mode till a lock is gained, if the "timeout" is a
+	positive value this is a period of time in seconds that this function will wait for a lock in a blocking mode.
+	If a non-positive value is set then this function will return immediately even if lock was not gained
+	:type timeout: int | float | None
+
+	:return: whether a specified lock is gained or not
+	:rtype: bool
+	"""
+	if timeout is None:
+		blocking = True
+		timeout = -1
+	elif timeout <= 0:
+		blocking = False
+		timeout = -1
+	else:
+		blocking = True
+	return lock.acquire(blocking=blocking, timeout=timeout)
+
+
+@verify_type('strict', timeout=(int, float, None), raise_exception=bool)
+@verify_value('strict', lock_fn=lambda x: callable(x))
+def critical_section_dynamic_lock(lock_fn, timeout=None, raise_exception=True):
 	""" Protect a function with a lock, that was get from the specified function. If a lock can not be acquire, then
 	no function call will be made
 
 	:param lock_fn: callable that returns a lock, with which a function may be protected
-	:param blocking: whenever to block operations with lock acquiring
-	:param timeout: timeout with which a lock acquiring will be made
+	:type lock_fn: callable
+
+	:param timeout: the same as 'timeout' in the :func:`.acquire_lock` function
+	:type timeout: int | float | None
+
 	:param raise_exception: whenever to raise an WCriticalSectionError exception if lock can not be acquired
+	:type raise_exception: bool
 
 	:return: decorator with which a target function may be protected
 	"""
 
-	if blocking is False or timeout is None:
-		timeout = -1
-
 	def first_level_decorator(decorated_function):
+		decorated_function.__lock_free_fn__ = decorated_function
+
 		def second_level_decorator(original_function, *args, **kwargs):
 			lock = lock_fn(*args, **kwargs)
-			if lock.acquire(blocking=blocking, timeout=timeout) is True:
+			if acquire_lock(lock, timeout=timeout) is True:
 				try:
 					result = original_function(*args, **kwargs)
 					return result
@@ -65,16 +97,14 @@ def critical_section_dynamic_lock(lock_fn, blocking=True, timeout=None, raise_ex
 	return first_level_decorator
 
 
-@verify_type('paranoid', blocking=bool, timeout=(int, float, None), raise_exception=bool)
-@verify_value('paranoid', timeout=lambda x: x is None or x > 0)
-@verify_type(lock=Lock().__class__)
-def critical_section_lock(lock=None, blocking=True, timeout=None, raise_exception=True):
-	""" An a wrapper for :func:`.critical_section_dynamic_lock` function call, but uses a static lock object
-	instead of a function that returns a lock with which a function protection will be made
+@verify_type('paranoid', timeout=(int, float, None), raise_exception=bool)
+@verify_type('strict', lock=Lock().__class__)
+def critical_section_lock(lock=None, timeout=None, raise_exception=True):
+	""" An a wrapper for :func:`.critical_section_dynamic_lock` function call, but with a static lock object
+	instead of a function that returns a lock
 
 	:param lock: lock with which a function will be protected
-	:param blocking: same as blocking in :func:`.critical_section_dynamic_lock` function
-	:param timeout: same as timeout in :func:`.critical_section_dynamic_lock` function
+	:param timeout: the same as 'timeout' in the :func:`.critical_section_dynamic_lock` function
 	:param raise_exception: same as raise_exception in :func:`.critical_section_dynamic_lock` function
 
 	:return: decorator with which a target function may be protected
@@ -84,34 +114,55 @@ def critical_section_lock(lock=None, blocking=True, timeout=None, raise_exceptio
 		return lock
 
 	return critical_section_dynamic_lock(
-		lock_fn=lock_getter, blocking=blocking, timeout=timeout, raise_exception=raise_exception
+		lock_fn=lock_getter, timeout=timeout, raise_exception=raise_exception
 	)
 
 
 class WCriticalResource:
 	""" Class for simplifying thread safety for a shared object. Each class instance holds its own lock object
 	that :meth:`.WCriticalResource.critical_section` method will be used to protect bounded methods.
-	The only assumption is only simple bounded methods are allowed to be protected by this class
+	Bounded methods are allowed to be protected only. Static methods or class methods are unsupported
 	"""
 
 	def __init__(self):
-		""" Create lock and return new object
+		""" Create a lock
 		"""
 		self.__lock = Lock()
 
 	def thread_lock(self):
-		""" Return lock with which a bounded methods may be protected
+		""" Return a lock with which a bounded methods may be protected
 
 		:return: threading.Lock
 		"""
 		return self.__lock
 
-	@staticmethod
-	@verify_type('paranoid', blocking=bool, timeout=(int, float, None), raise_exception=bool)
-	@verify_value('paranoid', timeout=lambda x: x is None or x > 0)
-	def critical_section(blocking=True, timeout=None, raise_exception=True):
+	@verify_type('paranoid', timeout=(int, float, None))
+	def critical_context(self, timeout=None):
+		""" Create a lock free context for this object
 
-		def lock_getter(self, *args, **kwargs):
+		:param timeout: the same as 'timeout' in the :meth:`.WLockFreeContext.__init__` method
+		:type timeout: int | float | None
+
+		:rtype: WLockFreeContext
+		"""
+		return WLockFreeContext(self, timeout=timeout)
+
+	@staticmethod
+	@verify_type('paranoid', timeout=(int, float, None), raise_exception=bool)
+	def critical_section(timeout=None, raise_exception=True):
+		""" Decorate a method with a lock protection (class methods and static methods are unsupported)
+
+		:param timeout: the same as 'timeout' in the :func:`.critical_section_dynamic_lock` function
+		:type timeout: int | float | None
+
+		:param raise_exception: the same as 'raise_exception' in the :func:`.critical_section_dynamic_lock`
+		function
+		:type raise_exception: bool
+
+		:rtype: bounded method
+		"""
+
+		def lock_fn(self, *args, **kwargs):
 			if isinstance(self, WCriticalResource) is False:
 				raise TypeError(
 					'Invalid object type. It must be inherited from WCriticalResource class and'
@@ -119,6 +170,75 @@ class WCriticalResource:
 				)
 			return self.thread_lock()
 
-		return critical_section_dynamic_lock(
-			lock_fn=lock_getter, blocking=blocking, timeout=timeout, raise_exception=raise_exception
-		)
+		return critical_section_dynamic_lock(lock_fn=lock_fn, timeout=timeout, raise_exception=raise_exception)
+
+
+class WLockFreeContext:
+	""" This class may be used as a context that gets a monopoly access to a critical resource
+	(an :class:`.WCriticalResource` object) and all the methods of that resource won't try to access already
+	gained lock and won't cause a dead lock.
+
+	:note: this object doesn't protect itself from a race condition. So this object usage must be limited to a
+	single thread only
+	"""
+
+	@verify_type('strict', critical_resource=WCriticalResource, timeout=(int, float, None))
+	def __init__(self, critical_resource, timeout=None):
+		""" Create a new context object
+
+		:param critical_resource: resource that this context should access and protect
+		:type critical_resource: WCriticalResource
+
+		:param timeout: the same as 'timeout' in the :func:`.acquire_lock` function
+		:type timeout: int | float | None
+		"""
+		self.__resource = critical_resource
+		self.__timeout = timeout
+		self.__unlock = False
+
+	def __getattr__(self, item):
+		""" "Proxy" an access to an original resource. If the result is protected by a lock with
+		:meth:`.WCriticalResource.critical_section` decorator then an original (lock free) method is returned
+
+		:raise WCriticalSectionError: if a resource was not locked before this access
+
+		:rtype: any
+		"""
+		if not self.__unlock:
+			raise WCriticalSectionError(
+				'A resource access error. A lock to the resource must be gotten at first!'
+			)
+
+		r_item = object.__getattribute__(self.__resource, item)
+
+		try:
+			return partial(r_item.__lock_free_fn__, r_item.__self__)
+		except AttributeError:
+			pass
+		return r_item
+
+	def __enter__(self):
+		""" Enter this context and lock a resource
+
+		:raise WCriticalSectionError: if a resource was not locked during a specified timeout
+
+		:rtype: WLockFreeContext
+		"""
+		if not acquire_lock(self.__resource.thread_lock(), self.__timeout):
+			raise WCriticalSectionError('Unable to lock a critical section')
+		self.__unlock = True
+		return self
+
+	def __exit__(self, exc_type, exc_val, exc_tb):
+		""" Exit a context and release a lock
+		"""
+		if self.__unlock is True:
+			self.__unlock = False
+			self.__resource.thread_lock().release()
+
+	def __del__(self):
+		""" If a resource is locked still then unlock it
+		"""
+		if self.__unlock is True:
+			self.__unlock = False
+			self.__resource.thread_lock().release()
